6bb6be1f4d7a217981dc5afffc48ad40d957b5e8
hash <hash@exultant.us>
2010-08-02 18:57:45 +0000

    JsonCodec now simply extends EonCodec by providing a new constructor that provides factories for JsonObject and JsonArray.  It still takes and returns Eon stuff from all of its functions, though.  (I'm not doing new generic types as I inherit downwards because I'm afraid of that typecasting up issue on the inner type.)

--- src/ahs/io/codec/eon/EonCodec.java
+++ src/ahs/io/codec/eon/EonCodec.java
@@ -6,34 +6,44 @@ import ahs.util.*;
 
 import java.util.*;
 
+/**
+ * @author hash
+ */
 public class EonCodec extends CodecImpl<EonObject> {
-	public EonCodec() {
+	public EonCodec(Factory<? extends EonObject> $objProvider, Factory<? extends EonArray> $arrProvider) {
 		super();
+		this.$objProvider = $objProvider;
+		this.$arrProvider = $arrProvider;
 	}
+
+	private Factory<? extends EonObject> $objProvider;
+	private Factory<? extends EonArray> $arrProvider;
 	
 	public static byte[] serialize(EonObject $eo) throws TranslationException {
 		return $eo.serialize();
 	}
 	public <$TARG> byte[] serialize($TARG $datclr) throws TranslationException {
-		return serialize(encode($datclr));
+		return encode($datclr).serialize();
 	}
 	
-	public static EonObject deserialize(EonObject $eo, byte[] $bar) throws TranslationException {
+	public EonObject deserialize(byte[] $bar) throws TranslationException {
+		EonObject $eo = $objProvider.make();
 		$eo.deserialize($bar);
 		return $eo;
 	}
-	public <$TARG> $TARG deserialize(EonObject $eo, byte[] $bar, Class<$TARG> $datclrclass) throws TranslationException {
-		return decode(deserialize($eo, $bar), $datclrclass);
+	public <$TARG> $TARG deserialize(byte[] $bar, Class<$TARG> $datclrclass) throws TranslationException {
+		return decode(deserialize($bar), $datclrclass);
 	}
 	
-	public static <$TYPE> EonArray encodeList(EonArray $ea, Codec<EonObject> $codec, List<$TYPE> $list) throws TranslationException {
+	public <$TYPE> EonArray encodeList(Codec<EonObject> $codec, List<$TYPE> $list) throws TranslationException {
+		EonArray $ea = $arrProvider.make();
 		int $size = $list.size();
 		for (int $i = 0; $i < $size; $i++)
 			$ea.put($i, $codec.encode($list.get($i)));
 		return $ea;
 	}
 	
-	public static <$TYPE> List<$TYPE> decodeList(Codec<EonObject> $codec, EonArray $ea, Class<$TYPE> $datclrclass) throws TranslationException {
+	public <$TYPE> List<$TYPE> decodeList(Codec<EonObject> $codec, EonArray $ea, Class<$TYPE> $datclrclass) throws TranslationException {
 		int $size = $ea.size();
 		List<$TYPE> $v = new ArrayList<$TYPE>($size);
 		for (int $i = 0; $i < $size; $i++)
--- src/ahs/io/codec/json/JsonCodec.java
+++ src/ahs/io/codec/json/JsonCodec.java
@@ -1,7 +1,7 @@
 package ahs.io.codec.json;
 
 import ahs.io.*;
-import ahs.io.codec.Codec.*;
+import ahs.io.codec.eon.*;
 import ahs.io.codec.*;
 import ahs.util.*;
 
@@ -11,73 +11,54 @@ import java.util.*;
 /**
  * @author hash
  */
-@Deprecated
-public class JsonCodec extends CodecImpl<JsonObject> {
+public class JsonCodec extends EonCodec {
 	public JsonCodec() {
-		super();
+		super(OBJPROVIDER, ARRPROVIDER);
 	}
 	
-	
-	
-	public static byte[] serialize(JsonObject $jo) throws TranslationException {
-		return $jo.toString().getBytes(Strings.UTF_8);
-	}
-	public <$TARG> byte[] serialize($TARG $datclr) throws TranslationException {
-		return serialize(encode($datclr));
-	}
-	
-	public static JsonObject deserialize(byte[] $bar) throws TranslationException {
-		return new JsonObject(new String($bar, Strings.UTF_8));
-	}
-	public <$TARG> $TARG deserialize(byte[] $bar, Class<$TARG> $datclrclass) throws TranslationException {
-		return decode(deserialize($bar), $datclrclass);
-	}
-	
-	public static <$TYPE> JsonArray encodeList(Codec<JsonObject> $codec, List<$TYPE> $list) throws TranslationException {
-		JsonArray $ja = new JsonArray();
-		int $size = $list.size();
-		for (int $i = 0; $i < $size; $i++)
-			$ja.put($codec.encode($list.get($i)));
-		return $ja;
-	}
-	
-	public static <$TYPE> List<$TYPE> decodeList(Codec<JsonObject> $codec, JsonArray $list, Class<$TYPE> $datclrclass) throws TranslationException {
-		int $size = $list.length();
-		List<$TYPE> $v = new ArrayList<$TYPE>($size);
-		for (int $i = 0; $i < $size; $i++)
-			$v.add($codec.decode($list.getObj($i), $datclrclass));
-		return $v;
-	}
+	private static final Factory<JsonObject>	OBJPROVIDER	= new Factory<JsonObject>() {
+										public JsonObject make() {
+											return new JsonObject();
+										}
+									};
+	private static final Factory<JsonArray>		ARRPROVIDER	= new Factory<JsonArray>() {
+										public JsonArray make() {
+											return new JsonArray();
+										}
+									};
 }
 
 
-// example of typical code body found within a class that provides its own codec.
-// note that often the encode and decode blocks will also involve references the private fields of TheTargetClass.
 
-///* BEGIN JSON CODEC BLOCK */
-//public static final Encoder<JsonObject,IdGlob> ENCODER_JSON;
-//public static final Decoder<JsonObject,IdGlob> DECODER_JSON;
-//static { JsonDencoder $t = new JsonDencoder(); ENCODER_JSON = $t; DECODER_JSON = $t; }
-//public static class JsonDencoder implements Dencoder<JsonObject,IdGlob> {
-//	public JsonObject encode(Codec<JsonObject> $codec, IdGlob $x) throws TranslationException {
-//		JsonObject $jo = new JsonObject();
-//		$jo.putKlass($x);
-//		$jo.put("p", $codec.encode($x.$pse));
-//		$jo.put("n", $codec.encode($x.$nth));
-//		$jo.put("k", Codec_Json.encodeList($codec, $x.$pkr));
-//		return $jo;
-//	}
-//	public IdGlob decode(Codec<JsonObject> $codec, JsonObject $jo) throws TranslationException {
-//		$jo.assertKlass(IdGlob.class);
-//		try {
-//			return new IdGlob(
-//					$codec.decode($jo.getJsonObject("p"), Pseudonym.class),
-//					$codec.decode($jo.getJsonObject("n"), Nethernym.class),
-//					new ArrayList<IbeKeyPrivate>(Codec_Json.decodeList($codec, $jo.getJsonArray("k"), IbeKeyPrivate.class))
-//			);
-//		} catch (Exception $e) {
-//			throw new TranslationException($e);
-//		}
-//	}
-//}
-///* END JSON CODEC BLOCK */
+// THIS SHIT IS OLD:
+
+	// example of typical code body found within a class that provides its own codec.
+	// note that often the encode and decode blocks will also involve references the private fields of TheTargetClass.
+	
+	///* BEGIN JSON CODEC BLOCK */
+	//public static final Encoder<JsonObject,IdGlob> ENCODER_JSON;
+	//public static final Decoder<JsonObject,IdGlob> DECODER_JSON;
+	//static { JsonDencoder $t = new JsonDencoder(); ENCODER_JSON = $t; DECODER_JSON = $t; }
+	//public static class JsonDencoder implements Dencoder<JsonObject,IdGlob> {
+	//	public JsonObject encode(Codec<JsonObject> $codec, IdGlob $x) throws TranslationException {
+	//		JsonObject $jo = new JsonObject();
+	//		$jo.putKlass($x);
+	//		$jo.put("p", $codec.encode($x.$pse));
+	//		$jo.put("n", $codec.encode($x.$nth));
+	//		$jo.put("k", Codec_Json.encodeList($codec, $x.$pkr));
+	//		return $jo;
+	//	}
+	//	public IdGlob decode(Codec<JsonObject> $codec, JsonObject $jo) throws TranslationException {
+	//		$jo.assertKlass(IdGlob.class);
+	//		try {
+	//			return new IdGlob(
+	//					$codec.decode($jo.getJsonObject("p"), Pseudonym.class),
+	//					$codec.decode($jo.getJsonObject("n"), Nethernym.class),
+	//					new ArrayList<IbeKeyPrivate>(Codec_Json.decodeList($codec, $jo.getJsonArray("k"), IbeKeyPrivate.class))
+	//			);
+	//		} catch (Exception $e) {
+	//			throw new TranslationException($e);
+	//		}
+	//	}
+	//}
+	///* END JSON CODEC BLOCK */
