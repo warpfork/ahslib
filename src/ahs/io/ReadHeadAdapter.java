2ce456f589557fd9d772302278286eef741b1697
hash <hash@exultant.us>
2010-08-06 07:11:55 +0000

    Okay, that was much easier than I expected.  I ended up deferring realization of EOF being reached until the around after that possible final partial chunk is read, and that just made the whole thing much more beautiful.

--- src/ahs/io/ReadHeadAdapter.java
+++ src/ahs/io/ReadHeadAdapter.java
@@ -1,12 +1,135 @@
 package ahs.io;
 
+import ahs.util.*;
+import ahs.util.thread.*;
+
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+
 /**
  * This abstraction allows one to make a whole ReadHead simply by specifying some details
- * of the pump body.
+ * of the pump body and the close method.
  * 
  * @author hash
  * 
  */
-public class ReadHeadAdapter {
+public abstract class ReadHeadAdapter<$T> implements ReadHead<$T> {
+	protected ReadHeadAdapter() {
+		this.$pipe = new Pipe<$T>();
+		this.$pump = new PumpT();	
+	}
+	
+	/**
+	 * This method is called once per cycle of the pump. It should read and construct
+	 * a chunk from the underlying stream/channel/whatever and return it. A null
+	 * return indicates EOF.
+	 */
+	protected abstract $T getChunk() throws IOException;
+	
+	private final PumpT			$pump;
+	private final Pipe<$T>			$pipe;
+	private Listener<ReadHead<$T>>		$el;
+	private ExceptionHandler<IOException>	$eh;
+	
+	public Pump getPump() {
+		return $pump;
+	}
+
+	public void setExceptionHandler(ExceptionHandler<IOException> $eh) {
+		this.$eh = $eh;
+	}
+
+	public void setListener(Listener<ReadHead<$T>> $el) {
+		this.$el = $el;
+	}
+	
+	public $T read() {
+		return $pipe.SRC.read();
+	}
+	
+	public $T readNow() {
+		return $pipe.SRC.readNow();
+	}
+	
+	public boolean hasNext() {
+		return $pipe.SRC.hasNext();
+	}
+	
+	public List<$T> readAll() {
+		return $pipe.SRC.readAll();
+	}
+	
+	public List<$T> readAllNow() {
+		return $pipe.SRC.readAllNow();
+	}
+	
+	public boolean isClosed() {
+		return $pipe.SRC.isClosed();
+	}
+	
+	/**
+	 * this function is for updating our state at this level to fully closed (i.e.,
+	 * after the event has propagated through the underlying stream and buffering, and
+	 * we will never add data to the readable buffer).
+	 */
+	private void ourClose() throws IOException {
+		close();	// this is likely redundant, but can't hurt.
+		
+		try {
+			$pipe.SRC.close();	// this transparently handles interruption of any still-blocking reads as well as return of the final readAll.
+		} catch (IOException $e) {
+			/* this can't actually happen in a pipe */
+		}
+		
+		// give our listener a chance to notice our closure.  (pipe doesn't know our listener.)  (our isClosed method refers to pipe, which already considers itself completely closed.)
+		Listener<ReadHead<$T>> $dated_el = $el;
+		if ($dated_el != null) $dated_el.hear(this);
+	}
+	
+	
+	
+	private class PumpT implements Pump {
+		public boolean isDone() {
+			return isClosed();
+		}
+		
+		public synchronized void run(final int $times) {
+			ExceptionHandler<IOException> $dated_eh = null;
+			for (int $i = 0; $i < $times; $i++) {
+				if (isDone()) break;
+
+				$T $chunk = null;
+				try {
+					$chunk = getChunk();
+				} catch (IOException $e) {
+					$dated_eh = $eh;
+					if ($dated_eh != null) $dated_eh.hear($e);
+					// i guess it's somewhat debatable whether or not any exception should close... but that's what InputStream does, so i'm sticking to it for the time 
+				}
+				
+				if ($chunk == null) {
+					try {
+						ourClose();	// it is in fact ok to flag ourselves as closed here, since regardless of how we got here the underlying stream is already not willing to give us more data
+					} catch (IOException $e) {
+						if ($dated_eh != null) $dated_eh = $eh;		// if we didn't get it earlier in this cycle, get it now; if we did, keep that one
+						if ($dated_eh != null) $dated_eh.hear($e);	// now tell 'em
+					}
+				} else {
+					// wrap it up and enqueue to the buffer
+					// readers will immediately Notice the new data due to the pipe's internal semaphore doing its job
+					try {
+						$pipe.SINK.write($chunk);
+					} catch (IOException $e) {
+						/* this can't actually happen in a pipe */
+					}
+					
+					// signal that we got a new chunk in
+					Listener<ReadHead<$T>> $dated_el = $el;
+					if ($dated_el != null) $dated_el.hear(ReadHeadAdapter.this);
+				}
+			}
+		}
+	}
 	
 }
--- src/ahs/io/ReadHeadStreamToByteBuffer.java
+++ src/ahs/io/ReadHeadStreamToByteBuffer.java
@@ -7,79 +7,40 @@ import java.io.*;
 import java.nio.*;
 import java.util.*;
 
-public class ReadHeadStreamToByteBuffer implements ReadHead<ByteBuffer> {
+public class ReadHeadStreamToByteBuffer extends ReadHeadAdapter<ByteBuffer> {
 	public ReadHeadStreamToByteBuffer(InputStream $base, int $blockSize) {
+		super();
 		this.$base = $base;
 		this.$blockSize = $blockSize;
-		this.$pipe = new Pipe<ByteBuffer>();
-		this.$pump = new PumpT();
 	}
 	
 	private final InputStream			$base;
 	private final int				$blockSize;
-	private final PumpT				$pump;
-	private final Pipe<ByteBuffer>			$pipe;
-	private Listener<ReadHead<ByteBuffer>>		$el;
-	private ExceptionHandler<IOException>		$eh;
 	
-	public Pump getPump() {
-		return $pump;
-	}
-
-	public void setExceptionHandler(ExceptionHandler<IOException> $eh) {
-		this.$eh = $eh;
-	}
-
-	public void setListener(Listener<ReadHead<ByteBuffer>> $el) {
-		this.$el = $el;
-	}
-	
-	public ByteBuffer read() {
-		return $pipe.SRC.read();
-	}
-	
-	public ByteBuffer readNow() {
-		return $pipe.SRC.readNow();
-	}
-	
-	public boolean hasNext() {
-		return $pipe.SRC.hasNext();
-	}
-	
-	public List<ByteBuffer> readAll() {
-		return $pipe.SRC.readAll();
-	}
-	
-	public List<ByteBuffer> readAllNow() {
-		return $pipe.SRC.readAllNow();
-	}
-	
-	public boolean isClosed() {
-		return $pipe.SRC.isClosed();
+	protected ByteBuffer getChunk() throws IOException {
+		byte[] $bats = new byte[$blockSize];
+		int $actualSizeRead, $currentSum = 0;
+		
+		// try to get as much as we can
+		while ($currentSum < $blockSize) {
+			$actualSizeRead = $base.read($bats, $currentSum, $blockSize-$currentSum);
+			if ($actualSizeRead == -1)	// EOF
+				if ($currentSum != 0)
+					break;	// this may seem a bit odd, but we're actually going to wait for the next full tick to report the EOF.  we need to use this tick to report the partial chunk we've already got.
+				else
+					return null;
+			else
+				$currentSum += $actualSizeRead;
+		}
+		
+		return ByteBuffer.wrap($bats, 0, $currentSum);
 	}
 	
 	public void close() throws IOException {
 		$base.close();
 	}
 	
-	/**
-	 * this function is for updating our state at this level to fully closed (i.e.,
-	 * after the event has propagated through the underlying stream and buffering, and
-	 * we will never add data to the readable buffer).
-	 */
-	private void ourClose() throws IOException {
-		$base.close();	// this is likely redundant, but can't hurt.
-		
-		try {
-			$pipe.SRC.close();	// this transparently handles interruption of any still-blocking reads as well as return of the final readAll.
-		} catch (IOException $e) {
-			/* this can't actually happen in a pipe */
-		}
-		
-		// give our listener a chance to notice our closure.  (pipe doesn't know our listener.)  (our isClosed method refers to pipe, which already considers itself completely closed.)
-		Listener<ReadHead<ByteBuffer>> $dated_el = $el;
-		if ($dated_el != null) $dated_el.hear(this);
-	}
+
 	
 	/**
 	 * <p>
@@ -102,60 +63,5 @@ public class ReadHeadStreamToByteBuffer implements ReadHead<ByteBuffer> {
 		}
 		$v.flip();
 		return $v;
-	}
-	
-	private class PumpT implements Pump {
-		public boolean isDone() {
-			return isClosed();
-		}
-		
-		public synchronized void run(final int $times) {
-			byte[] $bats;
-			int $actualSizeRead, $currentSum;
-			boolean $die = false;
-			ExceptionHandler<IOException> $dated_eh = null;
-			for (int $i = 0; $i < $times; $i++) {
-				if (isDone()) break;
-				
-				// try to get as much as we can
-				$bats = new byte[$blockSize];
-				$currentSum = 0;
-				try {
-					while ($currentSum < $blockSize) {
-						$actualSizeRead = $base.read($bats, $currentSum, $blockSize-$currentSum);
-						if ($actualSizeRead == -1) {	// EOF
-							$die = true;
-							break;
-						} else
-							$currentSum += $actualSizeRead;
-					}
-				} catch (IOException $e) {
-					$dated_eh = $eh;
-					if ($dated_eh != null) $dated_eh.hear($e);
-					$die = true;
-					// i guess it's somewhat debatable whether or not any exception should close... but that's what InputStream does, so i'm sticking to it for the time 
-				}
-				
-				// wrap it up and enqueue to the buffer
-				// readers will immediately Notice the new data due to the pipe's internal semaphore doing its job
-				if ($currentSum > 0) try {
-					$pipe.SINK.write(ByteBuffer.wrap($bats, 0, $currentSum));
-				} catch (IOException $e) {
-					/* this can't actually happen in a pipe */
-				}
-				
-				// signal that we got a new chunk in
-				Listener<ReadHead<ByteBuffer>> $dated_el = $el;
-				if ($dated_el != null) $dated_el.hear(ReadHeadStreamToByteBuffer.this);
-				
-				// this is roughly atomic in that no other writes should be able to happen before this since we're the only thread pumping
-				if ($die) try {
-					ourClose();	// it is in fact ok to flag ourselves as closed here, since regardless of how we got here the underlying stream is already not willing to give us more data
-				} catch (IOException $e) {
-					if ($dated_eh != null) $dated_eh = $eh;		// if we didn't get it earlier in this cycle, get it now; if we did, keep that one
-					if ($dated_eh != null) $dated_eh.hear($e);	// now tell 'em
-				}
-			}
-		}
 	}
 }
