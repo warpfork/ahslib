1086046d1d50316d5c90bdb62d4db594e7ee38bb
hash <hash@exultant.us>
2010-11-15 18:08:19 +0000

    Added some more documentation to what the PumperSelector is doing to control the Selector.

--- src/ahs/util/thread/PumperSelector.java
+++ src/ahs/util/thread/PumperSelector.java
@@ -9,10 +9,20 @@ import java.nio.channels.*;
 import java.util.*;
 
 /**
+ * <p>
+ * This class provides a comprehensive wrapper (more of an asylum, really) around
+ * java.nio.channels.Selector that simplifies common operation and provides efficient,
+ * thread-safe controls that the java Selector fails to provide. Using this class, it
+ * becomes possible to register and deregister selectable channels from any thread at any
+ * time safely.
+ * </p>
+ * 
+ * <p>
  * If using this with a ReadHead that is backed by NIO channels, you'll probably be
  * handing a reference to this pumper to the ReadHead; ReadHead don't typically expose the
  * details of their base enough for this class to do its job if merely given the public
  * interface of the ReadHead.
+ * </p>
  * 
  * @author hash
  * 
@@ -38,8 +48,14 @@ public class PumperSelector implements Pumper {
 	private final Pipe<Event>	$pipe;
 	
 	/**
+	 * <p>
+	 * Gets a "default" PumperSelector that is a singleton to the VM.
+	 * </p>
+	 * 
+	 * <p>
 	 * This method uses lazy instantiation which is NOT thread-safe... but it's fine
 	 * as long as it's used once from a single thread first.
+	 * </p>
 	 * 
 	 * @return the default single PumperSelector for this VM (already started in its
 	 *         own daemon thread).
@@ -51,6 +67,7 @@ public class PumperSelector implements Pumper {
 		}
 		return $default;
 	}
+	
 	private static PumperSelector	$default;
 	
 	/**
@@ -62,7 +79,20 @@ public class PumperSelector implements Pumper {
 		$t.start();
 	}
 	
-	/** {@inheritDoc} */
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * <p>
+	 * The method cycles through two phases. In the first phase blocks on the
+	 * selector, then runs the Pump associated with any channel that reports an event.
+	 * The Pump is run within the selection thread and told to get as much data as it
+	 * can -- it is expected that the Pump will return immediately when it runs out of
+	 * available data so that the selection thread continues to operate in an
+	 * nonblocking fashion overall. In the second phase, the internal event buffer
+	 * used to synchronize registration and deregistration requests from other threads
+	 * is checked, and any buffered events are processed.
+	 * </p>
+	 */
 	public void run() {
 		while (true) {
 			try {
@@ -72,7 +102,7 @@ public class PumperSelector implements Pumper {
 				X.cry($e);
 			}
 			
-			{	// Get list of selection keys with pending events
+			{ // Get list of selection keys with pending events
 				Iterator<SelectionKey> $itr = $selector.selectedKeys().iterator();
 				
 				// Process each key
@@ -82,7 +112,7 @@ public class PumperSelector implements Pumper {
 					
 					if (!$k.isValid()) continue;
 					
-					((Pump)$k.attachment()).run(Integer.MAX_VALUE);	// the pump is told to get as much as it can, but with the expectation that it will return much sooner (namely, when the channel runs out of immediately available data).
+					((Pump) $k.attachment()).run(Integer.MAX_VALUE); // the pump is told to get as much as it can, but with the expectation that it will return much sooner (namely, when the channel runs out of immediately available data).
 				}
 			}
 			
@@ -90,24 +120,20 @@ public class PumperSelector implements Pumper {
 			for (Event $evt : $evts) {
 				if ($evt instanceof Event_Reg) {
 					try {
-						if ($evt.channel() instanceof ServerSocketChannel)
-							$evt.channel().register($selector, SelectionKey.OP_ACCEPT, $evt.pump());
-						else
-							$evt.channel().register($selector, SelectionKey.OP_READ, $evt.pump());
+						if ($evt.channel() instanceof ServerSocketChannel) $evt.channel().register($selector, SelectionKey.OP_ACCEPT, $evt.pump());
+						else $evt.channel().register($selector, SelectionKey.OP_READ, $evt.pump());
 					} catch (ClosedChannelException $e) {
-						X.cry($e);	//XXX: i'm not sure if this is okay... what if the remote connection closes the connection between the register call and when the event comes out of the pipe here?
+						X.cry($e); //XXX: i'm not sure if this is okay... what if the remote connection closes the connection between the register call and when the event comes out of the pipe here?
 					}
 				} else if ($evt instanceof Event_Dereg) {
-					if ($evt.channel() == null)
-						for (Iterator<SelectionKey> $itr = $selector.keys().iterator(); $itr.hasNext();) {
-							SelectionKey $k = $itr.next();
-							if ($evt.$pump == $k.attachment()) $k.cancel();
-						}
-					else
-						for (Iterator<SelectionKey> $itr = $selector.keys().iterator(); $itr.hasNext();) {
-							SelectionKey $k = $itr.next();
-							if ($evt.$thing == $k.channel()) $k.cancel();
-						}
+					if ($evt.channel() == null) for (Iterator<SelectionKey> $itr = $selector.keys().iterator(); $itr.hasNext();) {
+						SelectionKey $k = $itr.next();
+						if ($evt.$pump == $k.attachment()) $k.cancel();
+					}
+					else for (Iterator<SelectionKey> $itr = $selector.keys().iterator(); $itr.hasNext();) {
+						SelectionKey $k = $itr.next();
+						if ($evt.$thing == $k.channel()) $k.cancel();
+					}
 				}
 			}
 		}
@@ -132,21 +158,34 @@ public class PumperSelector implements Pumper {
 	}
 	
 	private static abstract class Event {
-		protected Event(SelectableChannel $thing, Pump $pump) { this.$thing = $thing; this.$pump = $pump; }
-		private SelectableChannel $thing;
-		private Pump $pump;
-		public SelectableChannel channel() { return $thing; };
-		public Pump pump() { return $pump; };
+		protected Event(SelectableChannel $thing, Pump $pump) {
+			this.$thing = $thing;
+			this.$pump = $pump;
+		}
+		
+		private SelectableChannel	$thing;
+		private Pump			$pump;
+		
+		public SelectableChannel channel() {
+			return $thing;
+		};
+		
+		public Pump pump() {
+			return $pump;
+		};
 	}
+	
 	private static class Event_Reg extends Event {
 		private Event_Reg(SelectableChannel $ch, Pump $p) {
 			super($ch, $p);
 		}
 	}
+	
 	private static class Event_Dereg extends Event {
 		private Event_Dereg(Pump $p) {
 			super(null, $p);
 		}
+		
 		private Event_Dereg(SelectableChannel $ch) {
 			super($ch, null);
 		}
