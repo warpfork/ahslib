93ca15789812cab4a55c0d04f50092dddf297775
hash <hash@exultant.us>
2011-06-07 19:05:00 -0500

    Ant now correctly builds the codec-json module.

--- codec.json/main/src/us/exultant/ahs/io/codec/json/JsonException.java
+++ codec.json/main/src/us/exultant/ahs/io/codec/json/JsonException.java
@@ -1,12 +1,14 @@
 package us.exultant.ahs.io.codec.json;
 
+import us.exultant.ahs.io.*;
+
 /**
  * The JSONException is thrown by the JSON.org classes then things are amiss.
  * 
  * @author JSON.org
  * @version 2008-09-18
  */
-public class JsonException extends ahs.io.TranslationException {
+public class JsonException extends TranslationException {
 	public JsonException() {
 		super();
 	}
