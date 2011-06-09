ec52628dfb1b8cc0bb5dba921d484dc470f2dc61
hash <hash@exultant.us>
2011-06-09 15:33:30 -0500

    Applying attention to documentation again.
    I am now designing this documentation for inclusion directly within the exultant.us site.  This may raise some degree of inconvenience for generation of the completely styled documentation from a AHSlib repository clone alone; however, the content itself should remain completely legible without said styling fully applied, and most critically the javadocs will remain unaffected.  In the future I hope to produce a handier abstraction that will keep more separation between this documentation and its primary residence on the exultant.us site, but for the present that priority is low.

---
+++ dox/index.php
@@ -0,0 +1,26 @@
+<?php require($_SERVER['DOCUMENT_ROOT']."/style/head.php"); ?>
+<?php echo head("exultant.us AHSlib"); ?>
+<?php echo theme(); ?>
+<link rel='stylesheet' href='style.css' type='text/css'>
+<html id='ahslib-dox'><body class='std'>
+
+<h1 style='float:right; margin: 0.5em 8em;'>And How Simple!</h1>
+
+<p>
+	The "And How Simple!" library provides open-source Java solutions for rapid development of applications utilizing networking, serialization/persistence, concurrency control, and thread scheduling.  The primary goals of AHSlib are remove common drudgery from coding in java wherever possible, make writing multithreaded applications easier, and codify common design patterns so that good code becomes more self-documenting.  AHSlib  is tested for both stability and performance and comes complete with an abundance of abstract interfaces that made it sufficient to provide services to a diverse range of projects.
+</p>
+<p>
+	The AHS library is free and open source.  The codec modules are released under <a href=http://www.apache.org/licenses/LICENSE-2.0>Apache License 2.0</a>; all other modules of the library are released under <a href=http://www.gnu.org/licenses/lgpl.html>GNU LGPL v3</a>.  The AHS library contains some components derived from other open sources; see the code for their attached licenses.
+</p>
+<p>
+	AHSlib's primary source repository is held by Git and managed with Redmine &mdash; check it out: <a href='https://rubicon.exultant.us/redmine/projects/ahslib'>https://rubicon.exultant.us/redmine/projects/ahslib</a>
+</p>
+<p>
+	Documentation for new users is available <a href='intro'>here</a>.
+</p>
+<p>
+	This documentation is in the repository itself, so you can both read <i>and</i> contribute to it via git!
+</p>
+
+</body></html>
+<?php echo foot(); ?>
