27c98a4d531f6356c1297840520f731ae6f04035
hash <hash@exultant.us>
2011-06-26 03:39:29 -0500

    Improved javadocs throughout the Pipe class in the Thread module.

--- src/us/exultant/ahs/thread/Pipe.java
+++ src/us/exultant/ahs/thread/Pipe.java
@@ -6,65 +6,167 @@ import java.io.*;
 import java.util.*;
 import java.util.concurrent.*;
 
+/**
+ * <p>
+ * This class behaves almost exactly as per ConcurrentLinkedQueue (and is implemented
+ * using it), but also provides additional semantics that allow one to determine at least
+ * how many entries are in the queue as a constant time operation, and also provides a
+ * blocking get functionality. This allows easy use as a work distributor, since one can
+ * make get calls that will not return null simply because work is not instantaneously
+ * available.
+ * </p>
+ * 
+ * <p>
+ * Requests that are blocking in nature are given fair ordering; nonblocking requests
+ * disregard this entirely.
+ * </p>
+ * 
+ * @author hash
+ * 
+ */
 public class Pipe<$T> implements Flow<$T> {
+	/**
+	 * Constructs a new, open, active, empty, usable Pipe.
+	 */
 	public Pipe() {
 		$closed = new boolean[] { false };
 		$queue = new ConcurrentLinkedQueue<$T>();
-		$gate = new InterruptableSemaphore(0, true);	// fair.
+		$gate = new InterruptableSemaphore(0, true); // fair.
 		SRC = new Source();
 		SINK = new Sink();
 	}
 	
-	// i'm perfectly capable of using just the ReadHead and WriteHead interfaces internally as well... but this lets clients avoid having to always wrap the write calls in a no-op try-catch block that actually happens to be unreachable.
+	/**
+	 * <p>
+	 * The source from which one reads data from the pipe.
+	 * </p>
+	 * 
+	 * <p>
+	 * Note that it would be perfectly possible to use just the ReadHead and WriteHead
+	 * interfaces internally as well... but this lets clients avoid having to always
+	 * wrap the write calls in a no-op try-catch block that actually happens to be
+	 * unreachable.
+	 * </p>
+	 */
 	public final Source			SRC;
+	
+	/**
+	 * <p>
+	 * The sink to which one writes data into the pipe.
+	 * </p>
+	 * 
+	 * <p>
+	 * Note that it would be perfectly possible to use just the ReadHead and WriteHead
+	 * interfaces internally as well... but this lets clients avoid having to always
+	 * wrap the write calls in a no-op try-catch block that actually happens to be
+	 * unreachable.
+	 * </p>
+	 */
 	public final Sink			SINK;
+	
+	/**
+	 * <p>
+	 * This Listener is triggered for every completed write operation and for close
+	 * operations.
+	 * </p>
+	 */
 	private volatile Listener<ReadHead<$T>>	$el;
 	
 	/**
-	 * This is the buffer itself. Synchronizing on this basically is the write lock.
+	 * <p>
+	 * This is the data-containing buffer itself.
+	 * </p>
+	 * 
+	 * <p>
+	 * Synchronizing on this is the write lock.
+	 * </p>
 	 */
 	private final ConcurrentLinkedQueue<$T>	$queue;
+	
 	/**
-	 * we -always- update this -before- the queue so that it's a -minimal- value. it
-	 * may sometimes severely underestimate the available work in order to provide
-	 * this service with guaranteeable correctness, since reading the amount of
-	 * available work from this semaphore is an operation that is never blocked. (this
-	 * comes up when operations that may potentially modify the entire queue are in
-	 * progress. the more common operations that simply effect the head or tail of the
-	 * queue will not lead to drastic varation in the number of permits available to
-	 * the semphore).
+	 * <p>
+	 * We always update this semaphore so that it's a <i>minimal</i> value &mdash;
+	 * that is, permits are not released until <i>after</i> an insertion into the
+	 * queue completes, and permits are acquired <i>before</i> any read from the queue
+	 * is attempted. Because of this policy, the semaphore may sometimes severely
+	 * underestimate the available work in order to provide this service with
+	 * guaranteeable correctness, since reading the amount of available work from this
+	 * semaphore is an operation that is never blocked. (Operations that simply effect
+	 * the head or tail of the queue are not likely to lead to drastic varation in the
+	 * number of permits available to the semphore).
+	 * </p>
 	 * 
-	 * when write is locked, it can't grow (but it can still shrink, even when read is
-	 * locked, because the semaphore is used to synchronize and order read requests
-	 * even before the read lock is invoked). thus, if write is locked and all permits
-	 * are then drained, read is effectly completely locked as well.
+	 * <p>
+	 * When write is locked, the number of permits in this semaphore cannot grow (but
+	 * it can still shrink, because the semaphore is used to synchronize and order
+	 * read requests and there is no actual direct read lock). Thus, if write is
+	 * locked and all permits are then drained, an effcetive read locked is attained.
+	 * </p>
 	 */
 	private final InterruptableSemaphore	$gate;
+	
+	/**
+	 * This array is of length one. Its only value describes whether or not this Pipe
+	 * is closed to new writes; {@link #$gate} is interrupted immediately after this
+	 * value is set to true when closing a Pipe. (An array is used here instead of a
+	 * boolean primitive directly in order to provide a monitor for synchronization.)
+	 */
 	private final boolean[]			$closed;
 	
+	/**
+	 * @return {@link #SRC}.
+	 */
 	public Source source() {
 		return SRC;
 	}
-	
+
+	/**
+	 * @return {@link #SINK}.
+	 */
 	public Sink sink() {
 		return SINK;
 	}
 	
+	/**
+	 * The minimal amount of work immediately available (see the documentation of {@link #$gate} for more details).
+	 * 
+	 * @return the minimal amount of work immediately available.
+	 */
 	public int size() {
 		return $gate.availablePermits();
 	}
 	
+	
+	
+	/**
+	 * {@link Pipe}'s internal implementation of ReadHead.
+	 * 
+	 * @author hash
+	 *
+	 */
 	public final class Source implements ReadHead<$T> {
-		private Source() {}	// this should be a singleton per instance of the enclosing class
+		private Source() {} // this should be a singleton per instance of the enclosing class
 		
+		/**
+		 * @return null (as per the contract in {@link ReadHead#getPump()} for
+		 *         scenarios when the writing thread is effectively the pump).
+		 */
 		public Pump getPump() {
 			return null;
 		}
 		
+		/**
+		 * This method has no effect. Pipes are not capable of generating
+		 * IOException.
+		 */
 		public void setExceptionHandler(ExceptionHandler<IOException> $eh) {
-			// do nothing.  we're not even capable of having exceptions.
+			/* do nothing. */
 		}
 		
+		/**
+		 * Sets the Listener that will be triggered for every completed write
+		 * operation on the matching {@link Sink} and upon close.
+		 */
 		public void setListener(Listener<ReadHead<$T>> $el) {
 			Pipe.this.$el = $el;
 		}
@@ -73,7 +175,7 @@ public class Pipe<$T> implements Flow<$T> {
 			if (isClosed()) {
 				return readNow();
 			} else {
-				try {	
+				try {
 					// so... i need to acquire atomically with the closed check, or else acquire can happen immediately after an interrupt and end up blocking forever.
 					// the above is impossible.  so instead i just made the semaphore -always- throw interrupted exceptions after it's been interrupted once.
 					$gate.acquire();
@@ -117,12 +219,17 @@ public class Pipe<$T> implements Flow<$T> {
 			return $closed[0];
 		}
 		
+		/**
+		 * Closes the pipe. All blocked reads immediately return null following
+		 * this call, and all subsequent reads (blocking or nonblocking) will
+		 * return null following this call.
+		 */
 		public void close() {
 			synchronized ($queue) {
-				$closed[0] = true;	// set our state to closed
+				$closed[0] = true; // set our state to closed
 			}
-			$gate.interrupt();	// interrupt any currently blocking reads
-			X.notifyAll($closed);	// trigger the return of any final readAll calls
+			$gate.interrupt(); // interrupt any currently blocking reads
+			X.notifyAll($closed); // trigger the return of any final readAll calls
 			
 			// give our listener a chance to notice our closure.
 			Listener<ReadHead<$T>> $dated_el = $el;
@@ -138,9 +245,18 @@ public class Pipe<$T> implements Flow<$T> {
 	}
 	
 	public final class Sink implements WriteHead<$T> {
-		private Sink() {}	// this should be a singleton per instance of the enclosing class
+		private Sink() {} // this should be a singleton per instance of the enclosing class
 		
-		public void write($T $chunk) {
+		/**
+		 * Writes a single chunk of data to this Sink. After the data is commited,
+		 * a permit is released and the data becomes available for reading from
+		 * the {@link Source}, and the pipe's listener is then notified of the
+		 * event (note: the listener is called using the writing thread).
+		 * 
+		 * @throws IllegalStateException
+		 *                 if this Pipe is closed.
+		 */
+		public void write($T $chunk) throws IllegalStateException {
 			synchronized ($queue) {
 				if (isClosed()) throw new IllegalStateException("Pipe has been closed.");
 				$queue.add($chunk);
@@ -148,9 +264,29 @@ public class Pipe<$T> implements Flow<$T> {
 				
 				Listener<ReadHead<$T>> $el_dated = Pipe.this.$el;
 				if ($el_dated != null) $el_dated.hear(SRC);
+				//XXX:AHS:EFFIC:THREAD: I don't think we have to do this notification from within the write-lock, do we?
 			}
 		}
 		
+		/**
+		 * <p>
+		 * Writes every element of the given collection to this Sink. All elements
+		 * added as a group in this way are guaranteed to come out of the Pipe in
+		 * the same order as the original ordering of the collection, and shall
+		 * not be intermingled with other objects. Some elements added as a group
+		 * in this way may be read and cause notifications to the pipe's listener
+		 * before the entire group is added (so it may be possible for a thread
+		 * reading from a pipe in a non-blocking fashion to read half of the group
+		 * of elements, then get nulls, and then later return to see the other
+		 * half of the group).
+		 * </p>
+		 * 
+		 * <p>
+		 * Put another way: a single write-lock is maintained for the duration of
+		 * this method, but every element causes a release of permit and
+		 * notification to the pipe's listener.
+		 * </p>
+		 */
 		public void writeAll(Collection<? extends $T> $chunks) {
 			// at first i thought i could implement this with addAll on the queue and a single big release... not actually so.  addAll on the queue can throw exceptions but still have made partial progress.
 			// so while this is guaranteed to add all elements of the collection in their original order without interference by other threads, reading threads may actually be able to read the first elements from the collection before the last ones have been entered.
@@ -160,14 +296,26 @@ public class Pipe<$T> implements Flow<$T> {
 			}
 		}
 		
+		/**
+		 * Returns true. Pipe doesn't implement any capacity restrictions, so this
+		 * isn't really ever in question. isClosed is technically an unrelated
+		 * question.
+		 * 
+		 * @return true
+		 */
 		public boolean hasRoom() {
-			return true;	// we don't implement any capacity restrictions, so this isn't really ever in question.  isClosed is technically an unrelated question.
+			return true;
 		}
 		
 		public boolean isClosed() {
 			return $closed[0];
 		}
 		
+		/**
+		 * Closes the pipe. All blocked reads immediately return null following
+		 * this call, and all subsequent reads (blocking or nonblocking) will
+		 * return null following this call.
+		 */
 		public void close() {
 			SRC.close();
 		}
