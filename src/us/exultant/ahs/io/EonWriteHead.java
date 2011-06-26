4a2e285621174722bc1f0bceeeb6461c3915e4f9
hash <hash@exultant.us>
2011-06-26 01:03:29 -0500

    Ant now compiles and builds jars of the io module.
    I commented out some code that's related to both IO and codecs that's still a bit hung out to dry by this modularization quest; We'll have to bring that back into the fold later.

--- src/us/exultant/ahs/io/EonWriteHead.java
+++ src/us/exultant/ahs/io/EonWriteHead.java
@@ -1,31 +1,30 @@
-package us.exultant.ahs.io;
-
-import us.exultant.ahs.core.*;
-import us.exultant.ahs.codec.eon.*;
-import java.nio.channels.*;
-
-/**
- * Pretty much just a home for factories at this point.
- * 
- * FIXME: this functionality clearly belongs in IO more than codec... IO doesn't depend on codec yet, but i'm not opposed to allowing it to do so.
- * 
- * @author hash
- * 
- */
-public final class EonWriteHead {
-	public static WriteHead<EonObject> make(WritableByteChannel $wbc, EonCodec $co) {
-		return WriteHeadAdapter.make(
-				$wbc,
-				TranslatorStack.make(
-						new Eon.TranslatorToByteBuffer($co),
-						new WriteHeadAdapter.ChannelwiseUnbuffered.BabbleTranslator()
-				)
-		);
-	}
-	
-	
-	
-	
-	
-	private EonWriteHead() {}
-}
+//TODO:AHS:REFACTOR: these convenience methods can no longer exist here because of the modularization requirements.
+//package us.exultant.ahs.io;
+//
+//import us.exultant.ahs.core.*;
+//import us.exultant.ahs.codec.eon.*;
+//import java.nio.channels.*;
+//
+///**
+// * Pretty much just a home for factories at this point.
+// * 
+// * @author hash
+// * 
+// */
+//public final class EonWriteHead {
+//	public static WriteHead<EonObject> make(WritableByteChannel $wbc, EonCodec $co) {
+//		return WriteHeadAdapter.make(
+//				$wbc,
+//				TranslatorStack.make(
+//						new Eon.TranslatorToByteBuffer($co),
+//						new WriteHeadAdapter.ChannelwiseUnbuffered.BabbleTranslator()
+//				)
+//		);
+//	}
+//	
+//	
+//	
+//	
+//	
+//	private EonWriteHead() {}
+//}
