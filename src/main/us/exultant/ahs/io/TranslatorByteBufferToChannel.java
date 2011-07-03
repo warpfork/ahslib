7c08e5f2b7fcabd28e2a429d1f39b58d34c8bd25
hash <hash@exultant.us>
2011-07-02 20:58:37 -0500

    TranslatorByteBufferToChannel is now complete with both nonblocking and blocking implementations.

--- src/main/us/exultant/ahs/io/TranslatorByteBufferToChannel.java
+++ src/main/us/exultant/ahs/io/TranslatorByteBufferToChannel.java
@@ -8,62 +8,50 @@ import java.nio.channels.*;
 
 /**
  * <p>
- * This actually 'translates' bytes onto a communication channel. It follows the
- * dead-simple "babble" protocol: when given a series of arbitrary blobs, the bytes that
- * end up in the channel look like a series of 4-byte signed integer lengths, followed by
- * that blob of the size specified by the int.
+ * This 'writes' by actually 'translating' bytes onto a communication channel. It follows
+ * the dead-simple "babble" protocol: when given a series of arbitrary blobs, the bytes
+ * that end up in the channel look like a series of 4-byte signed integer lengths,
+ * followed by that blob of the size specified by the int.
  * </p>
  * 
  * <p>
- * Two different implementations of this are provided as nested classes; one does a
- * significantly better job of maintaining its promise to be nonblocking (but at the cost
- * of greater complexity for the client code), while the other is simpler to use but may
- * sometimes block.
+ * Two distinct implementations of this are provided as nested classes; one does a
+ * significantly better job of maintaining its promise to be nonblocking (
+ * {@link Nonblocking}) but at the cost of greater complexity for the client code, while
+ * the other is simpler to use but may sometimes block ({@link Blocking}).
  * </p>
  * 
  * @author hash
  * 
  */
-public abstract class TranslatorByteBufferToChannel {
+public abstract class TranslatorByteBufferToChannel implements Translator<ByteBuffer,TranslatorByteBufferToChannel.Completor> {
 	/**
 	 * <p>
 	 * This implementation of translating writes to channels is nonblocking. It
-	 * returns an object (a {@link Completor}) that is responsible for the given blob;
-	 * this object can be repeatedly invoked to nonblockingly write as much of the
-	 * blob as immediately possible until the blob is consumed. If several such
-	 * {@link Completor}s are produced regarding the same {@link WritableByteChannel},
-	 * as long as they are all consumed completely after their first
-	 * {@link Completor#write} call without writes called on any other Completors in
-	 * the meanwhile, everything works out fine.
+	 * returns an object (a {@link Completor}) that is responsible for the given blob.
 	 * </p>
 	 * 
 	 * <p>
-	 * (This class bears more resemblance to {@link TranslatorChannelToByteBuffer}
-	 * than does the {@link Blocking} implementation; TranslatorChannelToByteBuffer is
-	 * just able to do a better job of hiding its equivalent of {@link Completor}
-	 * because {@link TranslatorStack} allows it to return nulls if it isn't finished
-	 * with an entire semantic read (whereas {@link TranslatorStack} unfortunately has
-	 * no such route for permissiveness of incomplete writes).)
+	 * (This class bears close resemblance to
+	 * {@link TranslatorChannelToByteBuffer.Nonblocking} aside from the return type;
+	 * TranslatorChannelToByteBuffer is just able to do a better job of hiding its
+	 * equivalent of {@link Completor} because {@link TranslatorStack} allows it to
+	 * return nulls if it isn't finished with an entire semantic read (whereas
+	 * {@link TranslatorStack} unfortunately has no such route for permissiveness of
+	 * incomplete writes).)
 	 * </p>
 	 * 
 	 * @author hash
-	 * 
 	 */
-	public static class Nonblocking implements Translator<ByteBuffer,Nonblocking.Completor> {
-		public static class Completor {
-			
-			public int write() {
-				return -1;
-			}
-			
-			public boolean isComplete() {
-				return false;
-			}
+	public static class Nonblocking extends TranslatorByteBufferToChannel {
+		public Nonblocking(WritableByteChannel $base) {
+			this.$base = $base;
 		}
 		
+		private final WritableByteChannel	$base;
+		
 		public Completor translate(ByteBuffer $blob) throws TranslationException {
-			return null;
-			
+			return new Completor($base, $blob);
 		}
 	}
 	
@@ -83,40 +71,29 @@ public abstract class TranslatorByteBufferToChannel {
 	 * </p>
 	 * 
 	 * <p>
-	 * It is NOT possible to use the same instance of a TranslatorByteBufferToChannel
-	 * in several TranslationStack for any purpose if threads are involved.
-	 * </p>
-	 * 
-	 * <p>
-	 * (A note about some of the reasons why this implementation is not completely
-	 * nonblocking: it quite simply doesn't turn out anywhere near so elegant as
-	 * reading. You end up having to pass in the same thing several times, or else
-	 * having an alternate triggering mechanism, neither of which concepts play even
-	 * remotely nicely with the whole idea of making translator stacks. Thus, if you
-	 * want this nonblocking, you have to go out of your way to specify that by using
-	 * a different class entirely, since despite the fact that such a class might also
-	 * implements the same Translator interface, it would really screw with a lot of
-	 * the normal concepts of Translator.)
+	 * It is NOT possible to use the same instance of a
+	 * TranslatorByteBufferToChannel.Blocking in several TranslationStack for any
+	 * purpose if threads are involved.
 	 * </p>
 	 * 
 	 * @author hash
-	 * 
 	 */
-	public static class Blocking implements Translator<ByteBuffer,WritableByteChannel> {
+	public static class Blocking extends TranslatorByteBufferToChannel {
 		public Blocking(WritableByteChannel $base) {
 			this.$base = $base;
 		}
 		
 		private final WritableByteChannel	$base;
 		private final ByteBuffer		$preint	= ByteBuffer.allocate(4);
+		private static final Completor		$straw = new Completor();
 		
-		public WritableByteChannel translate(ByteBuffer $blob) throws TranslationException {
+		public Completor translate(ByteBuffer $blob) throws TranslationException {
 			$preint.clear();
 			$preint.putInt($blob.remaining());
 			$preint.rewind();
 			subwrite($preint);
 			subwrite($blob);
-			return $base;
+			return $straw;
 		}
 		
 		private void subwrite(ByteBuffer $lit) throws TranslationException {
@@ -129,6 +106,86 @@ public abstract class TranslatorByteBufferToChannel {
 			} catch (IOException $e) {
 				throw new TranslationException("problems with base channel", $e);
 			}
+		}
+	}
+	
+	
+	
+	/**
+	 * <p>
+	 * A Completor is responsible for commiting the entirety of a blob to a channel,
+	 * while allowing partial writes from every particular operation attempt.
+	 * </p>
+	 * 
+	 * <p>
+	 * A Completor should be repeatedly invoked to nonblockingly write as much of the
+	 * blob as immediately possible until the blob is consumed. If several
+	 * {@link Completor}s are produced regarding the same {@link WritableByteChannel},
+	 * as long as they are all consumed completely after their first
+	 * {@link Completor#write} call without writes called on any other competing
+	 * Completors in the meanwhile, everything works out fine.
+	 * </p>
+	 * 
+	 * <p>
+	 * Some implementations of TranslatorByteBufferToChannel may return no-op
+	 * Completors which are already "completed" without requiring any write operations
+	 * because they've already completed their writes in a blocking fashion (in other
+	 * words, such a Completor may be safely ignored, but may also be handled in the
+	 * exact same way as a fully functional completor).
+	 * </p>
+	 * 
+	 * @author hash
+	 */
+	public static class Completor {
+		private Completor(WritableByteChannel $chan, ByteBuffer $blob) {
+			this.$chan = $chan;
+			this.$blob = $blob;
+			this.$preint = ByteBuffer.allocate(4);
+			$preint.putInt($blob.remaining());
+			$preint.rewind();
+		}
+		private Completor() {
+			this.$chan = null;
+			this.$blob = null;
+			this.$preint = null;
+		}
+
+		private final WritableByteChannel $chan;
+		private final ByteBuffer $blob;
+		private final ByteBuffer $preint;
+		
+		/**
+		 * Immediately attempts to write as much data as possible to this
+		 * Completor's channel from its assigned blob.
+		 * 
+		 * @return the number of bytes of the blob written to channel; may be zero
+		 *         (if the channel was already full, or if there was only room to
+		 *         write the length header, or if this Completor's blob was
+		 *         already completely consumed).
+		 * @throws IOException
+		 *                 if the underlying channel throws IOException during a
+		 *                 write attempt.
+		 */
+		// the assymmetry between where exceptions bubble out from the blocking and nonblocking versions bothers me a bit.
+		public int write() throws IOException {
+			if ($blob == null) return -1;	// it's a strawman
+			int $v = 0;
+			if ($preint.remaining() > 0) $v += $chan.write($preint);	// we might have done this already in a previous invocation, but so?  the buffer remembers how much is remaining.
+			//ohgod: even if you're treating this channel single-threadedly in the jvm, the *kernel* is still draining at random, so you can't just happily jump onto trying to write the blob and assume that if the preint didn't finish writing then the blob will also fail properly.
+			if ($preint.remaining() > 0) return $v;
+			$v += $chan.write($blob);
+			return $v;
+		}
+		
+		/**
+		 * Determines whether this Completor is completely consumed.
+		 * 
+		 * @return true if there is no more data awaiting a chance to be written
+		 *         to channel; false otherwise.
+		 */
+		public boolean isComplete() {
+			if ($blob == null) return true;	// it's a strawman
+			return ($blob.remaining() == 0);
 		}
 	}
 }
