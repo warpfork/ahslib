0928a50c3bdb1b763ac18b11a7f4a36249da1816
hash <hash@exultant.us>
2011-07-02 21:53:01 -0500

    Implemented a generic no-op Translator.

--- src/main/us/exultant/ahs/core/Translator.java
+++ src/main/us/exultant/ahs/core/Translator.java
@@ -2,4 +2,20 @@ package us.exultant.ahs.core;
 
 public interface Translator<$FROM, $TO> {
 	public $TO translate($FROM $x) throws TranslationException;
+	
+	/**
+	 * Implements a no-op translator.
+	 * 
+	 * @author hash
+	 *
+	 * @param <$T>
+	 */
+	public static final class Noop<$T> implements Translator<$T,$T> {
+		/**
+		 * @return the same object as given, unmodified.
+		 */
+		public $T translate($T $x) {
+			return $x;
+		}
+	}
 }
