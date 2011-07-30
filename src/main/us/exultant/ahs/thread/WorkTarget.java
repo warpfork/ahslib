5ac3809b2c9843efab006231e26e74b4b4eeed30
hash <hash@exultant.us>
2011-07-30 17:59:45 -0500

    Added a concept of priority to WorkTarget.

--- src/main/us/exultant/ahs/thread/WorkTarget.java
+++ src/main/us/exultant/ahs/thread/WorkTarget.java
@@ -54,28 +54,28 @@ public interface WorkTarget {
 	 * based on the assumption that this sort of idiocy will not be performed.)
 	 * </p>
 	 */
-//	 * <p>
-//	 * This interface provides the the argument for specifying number of times to be
-//	 * run as a kindness to both WorkAtom implementations which will be able to be
-//	 * more efficient when allocating some resources or buffers in blocks and reusing
-//	 * them across cycles, and also as a kindness to WorkScheduler implementations
-//	 * which may be responsible for running multiple WorkAtom instances wanting to
-//	 * have some vague control over how its attention is batched. However, if the
-//	 * WorkAtom implementer is truly lazy, there's nothing that requires them to heed
-//	 * the <code>$times</code> argument at all.
-//	 * </p>
-//	 * 
-//	 * @param $times
-//	 *                the number of internal cycles the <code>WorkAtom</code> should
-//	 *                complete before returning from this call.
+	//	 * <p>
+	//	 * This interface provides the the argument for specifying number of times to be
+	//	 * run as a kindness to both WorkAtom implementations which will be able to be
+	//	 * more efficient when allocating some resources or buffers in blocks and reusing
+	//	 * them across cycles, and also as a kindness to WorkScheduler implementations
+	//	 * which may be responsible for running multiple WorkAtom instances wanting to
+	//	 * have some vague control over how its attention is batched. However, if the
+	//	 * WorkAtom implementer is truly lazy, there's nothing that requires them to heed
+	//	 * the <code>$times</code> argument at all.
+	//	 * </p>
+	//	 * 
+	//	 * @param $times
+	//	 *                the number of internal cycles the <code>WorkAtom</code> should
+	//	 *                complete before returning from this call.
 	// the whole scheme of batching things makes it impossible for this to implement Runnable, which is... well, it makes things ishy when i compose my WorkScheduler of ThreadPoolExecutor.
 	//  and really, if you have a WorkTarget doing atoms so small that it is a serious efficiency concern to process more than one atom at a time under a single lock... well, do it.  the scheduler doesn't actually really want to know about that (it did in mcon, but not everything is mcon).
 	public void run();
 	
 	/**
 	 * <p>
-	 * Implementers use this method to allow the WorkTarget's <code>run()</code> method
-	 * to know when to stop spinning.
+	 * Implementers use this method to allow the WorkTarget's <code>run()</code>
+	 * method to know when to stop spinning.
 	 * </p>
 	 * 
 	 * <p>
@@ -87,4 +87,48 @@ public interface WorkTarget {
 	 *         its earliest opportunity.
 	 */
 	public boolean isDone();
+	
+	/**
+	 * <p>
+	 * Estimates the priority with which this WorkTarget should be scheduled relative
+	 * to other WorkTarget at the same WorkScheduler; a higher priority indicates that
+	 * this WorkTarget should be scheduled preferentially.
+	 * </p>
+	 * 
+	 * <p>
+	 * WorkScheduler may use this priority to the best of its ability in scheduling
+	 * work for powering by the scheduler's threads, but the priority at any point in
+	 * time is at most a best-effort basis and not a guarantee of order due to the
+	 * concurrent nature of the scheduling and furthermore because a WorkScheduler
+	 * implmentation may balance concerns other than priority (such as tasks scheduled
+	 * based on wall-clock time).
+	 * </p>
+	 * 
+	 * <p>
+	 * This priority may change over time (this could be useful for example in a
+	 * program which has several work buffers and always wishes to service the fullest
+	 * one, or similiar the least recently served one); WorkScheduler who obey the
+	 * priority hint will do their best to respond to this in a timely manner every
+	 * time they are told to {@link WorkScheduler#update(WorkTarget)} their
+	 * relationship with this WorkTarget.
+	 * </p>
+	 * 
+	 * <p>
+	 * Another example of effective use of the priority system is a server application
+	 * which is composed of three types of task: nonblocking reads, some application
+	 * logic, and nonblocking writes of the result. By simply giving writes the
+	 * highest priority, application logic a medium priority, and reads the lowest
+	 * priority, the entire application becomes optomized to keep all of its pipes
+	 * between these task as small as possible, and furthermore if the reads happen to
+	 * be based on some system including a sliding window (namely, TCP), the TCP
+	 * buffer naturally fills in the OS kernel layer and causes the TCP window to
+	 * automatically adapt when the application level logic is over burdened &mdash;
+	 * all with no special effort from the application designer except for setting
+	 * those three priorities.
+	 * </p>
+	 * 
+	 * @return an integer representing the priority with which this WorkTarget should
+	 *         currently be considered if it has work ready.
+	 */
+	public int getPriority();
 }
