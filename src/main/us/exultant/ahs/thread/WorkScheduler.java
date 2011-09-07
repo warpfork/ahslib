8264018fc06d67cfa706b2c07e063e3a977398ae
hash <hash@exultant.us>
2011-09-06 22:41:51 -0500

    Javadoc for a method in WorkScheduler.  I just had an idea a couple minutes ago about APIs that I'm likely vastly better than what I've been doing so far following in the footsteps of DL, and it's gonna change things a bit.

--- src/main/us/exultant/ahs/thread/WorkScheduler.java
+++ src/main/us/exultant/ahs/thread/WorkScheduler.java
@@ -10,16 +10,39 @@ import java.util.concurrent.*;
  */
 public interface WorkScheduler {
 	/**
+	 * <p>
 	 * Arranges for a {@link WorkTarget} to be executed at the convenience of this
 	 * scheduler. The {@link Future#get()} method of the returned {@link Future} will
 	 * return once {@link WorkTarget#isDone()} returns true, yielding the last result
 	 * returned by the {@link WorkTarget#call()} method.
+	 * </p>
 	 * 
-	 * The WorkTarget will be checked for
+	 * <p>
+	 * The WorkTarget will be checked for readiness immediately upon call of this
+	 * method, and thereafter be run by a when the scheduler has resources available
+	 * according to the priority of the WorkTarget. If the WorkTarget is not ready at
+	 * the time of first scheduling, the scheduler is not required to run it until
+	 * after an invocation of {@link #update(WorkFuture)} (with an argument of the
+	 * WorkFuture returned by the first scheduling call) that occurs when the
+	 * WorkTarget is ready.
+	 * </p>
 	 * 
-	 * It is only appropriate to schedule a single WorkTarget with a single
+	 * <p>
+	 * If a WorkTarget represents a task which can be performed by several threads at
+	 * once (a common example being some sort of server can respond to one client
+	 * independantly per thread), then several instances of the same WorkTarget
+	 * implementation should be created and each of them scheduled &mdash one per
+	 * thread which should be able to respond. (If you want as many threads as
+	 * possible to be able to perform a type of work, consider creating a
+	 * {@link Factory} for the WorkTarget and using the
+	 * {@link WorkManager#scheduleOnePerCore(Factory,WorkScheduler)} helper method.)
+	 * </p>
+	 * 
+	 * <p>
+	 * It is only appropriate to schedule a single WorkTarget once and with a single
 	 * WorkScheduler at any one time. This condition is not checked in the code; it is
-	 * up to the developer to maintain sanity.  Note that if a WorkTarget leaves the scheduler by finishing with an exception (
+	 * up to the developer to maintain sanity.
+	 * </p>
 	 * 
 	 * @param <$V>
 	 *                The type returned by a WorkTarget. (This is often {@link Void}
@@ -37,8 +60,8 @@ public interface WorkScheduler {
 	public <$V> WorkFuture<$V> schedule(WorkTarget<$V> $wt, long $delay, TimeUnit $unit);
 	
 	public <$V> WorkFuture<$V> scheduleAtFixedRate(WorkTarget<$V> $wt, long $initialDelay, long $delay, TimeUnit $unit);
-	// so really, one has to admit that we could just wrap the clock stuff up with WorkTarget's isReady(), because, well, that just makes sense.  Well, except for the fact that that would make it really hard for us to sort them based on next-ready time, which is a pretty big deal.  so yes, we still do need a type that handles that explicitly.
+	
 	public <$V> WorkFuture<$V> scheduleWithFixedDelay(WorkTarget<$V> $wt, long $initialDelay, long $delay, TimeUnit $unit);
 	
-	public <$V> void update(WorkFuture<$V> $fut);	//TODO:AHS:THREAD:PLAN: so it's the future that's going to hold the magic data that lets us index into our heaps fast... but it's the worktarget that has, well, its own pointer to self, and is also what most other folks point to (the folks who are event listeners are the only ones that are super important).  so, that sucks.  soln?  hashmap, that's the best that can be done.  fortunately it only has to swim when we add or finish tasks rather than on every update or schedule or etc.  and most event listeners CAN get the Future pointer since they've been designed to be set after WT init, so really that's fine, just mildly annoying in its inelegance and unintuitiveness to someone new to the library.
+	public <$V> void update(WorkFuture<$V> $fut);
 }
