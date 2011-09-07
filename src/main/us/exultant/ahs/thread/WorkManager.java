e0e0a3372dac51c34b6d1d563c876aa30452b0a4
hash <hash@exultant.us>
2011-09-06 22:40:53 -0500

    Implement some handies in WorkManager.

--- src/main/us/exultant/ahs/thread/WorkManager.java
+++ src/main/us/exultant/ahs/thread/WorkManager.java
@@ -1,35 +1,49 @@
 package us.exultant.ahs.thread;
 
+import us.exultant.ahs.core.*;
+import java.util.concurrent.*;
+
 public class WorkManager {
-// factory methods for wrappers can also go in this class.
+	public static Future<Void> scheduleOnePerCore(Factory<WorkTarget<?>> $wtf) {
+		return scheduleOnePerCore($wtf, getDefaultScheduler());
+	}
+	public static Future<Void> scheduleOnePerCore(final Factory<WorkTarget<?>> $wtf, final WorkScheduler $ws) {
+		final int $n = Runtime.getRuntime().availableProcessors();
+		WorkFuture<?>[] $fa = new WorkFuture<?>[$n]; 
+		for (int $i = 0; $i < $n; $i++)
+			$fa[$i] = $ws.schedule($wtf.make());
+		return null;	//FIXME:AHS:THREAD: return an aggregate future
+	}
+	
+	
+	
+	// factory methods for wrappers can also go in this class.
 	
 	
 	
 	
 	
-	//TODO:AHS:THREAD: i'm really not sure what i'm going to do about defaults for this stuff.
+	//PLAN:AHS:THREAD: i'm really not sure what i'm going to do about defaults for this stuff.
 	//	core problem 1: not every program might want the same kind of WorkScheduler.  And while they can start their own, then we start spawning more threads than cores.  Not the end of the world (especially if the entire application ignores the default one, since then those threads are never run anyway), but less than ideal.
 	//	core problem 2: some programs might want to tweak performance settings -- changing the balance of thread allocation between priority bins, for example.  And those changes being global?  Urgh.
-	
-	
-//	/**
-//	 * <p>
-//	 * Gets a "default" WorkScheduler that is a singleton to the VM.
-//	 * </p>
-//	 * 
-//	 * <p>
-//	 * This method is performs lazy instantiation, is thread-safe, and the returned
-//	 * WorkScheduler is already started with its own complement of worker threads when
-//	 * returned.
-//	 * </p>
-//	 * 
-//	 * @return the single default WorkScheduler for this VM.
-//	 */
-//	public static WorkScheduler getDefault() {
-//		return SingletonHolder.INSTANCE;
-//	}
-//
-//	private static class SingletonHolder {
-//		public static final WorkScheduler INSTANCE = new WorkScheduler();
-//	}
+	/**
+	 * <p>
+	 * Gets a "default" WorkScheduler that is a singleton to the VM.
+	 * </p>
+	 * 
+	 * <p>
+	 * This method is performs lazy instantiation, is thread-safe, and the returned
+	 * WorkScheduler is already started with its own complement of worker threads when
+	 * returned.
+	 * </p>
+	 * 
+	 * @return the single default WorkScheduler for this VM.
+	 */
+	public static WorkScheduler getDefaultScheduler() {
+		return SingletonHolder.INSTANCE;
+	}
+
+	private static class SingletonHolder {
+		public static final WorkScheduler INSTANCE = new WorkSchedulerFlexiblePriority();
+	}
 }
