a84857a32dcfa97cd7299e5d1d622b134c6cfbea
hash <hash@exultant.us>
2011-09-30 16:32:35 -0700

    Synthesized a number of new unit tests for WorkScheduler implementations.  (They don't pass yet.)

--- src/test/us/exultant/ahs/thread/WorkSchedulerTest.java
+++ src/test/us/exultant/ahs/thread/WorkSchedulerTest.java
@@ -8,7 +8,7 @@ import java.util.concurrent.*;
 
 public abstract class WorkSchedulerTest extends TestCase {
 	public WorkSchedulerTest() {
-		super(new Logger(Logger.LEVEL_DEBUG), true);
+		super(new Logger(Logger.LEVEL_TRACE), true);
 	}
 	
 	public WorkSchedulerTest(Logger $log, boolean $enableConfirmation) {
@@ -20,7 +20,9 @@ public abstract class WorkSchedulerTest extends TestCase {
 		$tests.add(new TestRunOnce());
 		$tests.add(new TestWtAlwaysReady());
 		$tests.add(new TestNonblockingLeaderFollower());
+		$tests.add(new TestScheduleSingleDelayMany());
 		$tests.add(new TestScheduleFixedRate());
+		$tests.add(new TestNonblockingManyWorkSingleSource());
 		return $tests;
 	}
 	
@@ -172,10 +174,44 @@ public abstract class WorkSchedulerTest extends TestCase {
 	
 	
 	
+	/**  */
+	private class TestScheduleSingleDelayMany extends TestCase.Unit {
+		protected WorkScheduler $ws = makeScheduler();
+		public final int WTC = 8;
+		
+		public Object call() {
+			WorkFuture<?>[] $wf = Arr.newInstance(WorkFuture.class, WTC);
+			$wf[3] = $ws.schedule(new WorkTarget.RunnableWrapper(new Work(), 03, true), ScheduleParams.makeDelayed(400));
+			$wf[4] = $ws.schedule(new WorkTarget.RunnableWrapper(new Work(), -9, true), ScheduleParams.makeDelayed(500));
+			$wf[5] = $ws.schedule(new WorkTarget.RunnableWrapper(new Work(), 07, true), ScheduleParams.makeDelayed(600));
+			$wf[0] = $ws.schedule(new WorkTarget.RunnableWrapper(new Work(), 00, true), ScheduleParams.makeDelayed(100));
+			$wf[1] = $ws.schedule(new WorkTarget.RunnableWrapper(new Work(), 40, true), ScheduleParams.makeDelayed(200));
+			$wf[2] = $ws.schedule(new WorkTarget.RunnableWrapper(new Work(), 17, true), ScheduleParams.makeDelayed(300));
+			$wf[6] = $ws.schedule(new WorkTarget.RunnableWrapper(new Work(), 30, true), ScheduleParams.makeDelayed(700));
+			$wf[7] = $ws.schedule(new WorkTarget.RunnableWrapper(new Work(), -6, true), ScheduleParams.makeDelayed(800));
+			
+			try {
+				for (int $i = 1; $i < WTC; $i++) {
+					$wf[$i-1].get();
+					assertFalse($wf[$i].isDone());
+				}
+			}
+			catch (InterruptedException $e) { throw new AssertionFailed($e); }
+			catch (ExecutionException $e) { throw new AssertionFailed($e); }
+			
+			return null;
+		}
+		private class Work implements Runnable {
+			public void run() {}	// the run-once functionality is just provided by the RunnableWrapper clas.
+		}
+	}
+	
+	
+	
 	/** One task with a fixed delay is scheduled to run 10 times, and is checked by another thread (at fixed delay, awkwardly, but the resolution is low enough that it's kay). */
 	private class TestScheduleFixedRate extends TestCase.Unit {
 		protected WorkScheduler $ws = makeScheduler();
-
+		
 		public Object call() {
 			Work $wt = new Work();
 			WorkFuture<Integer> $wf = $ws.schedule($wt, ScheduleParams.makeFixedDelay(300, 100));
@@ -198,8 +234,8 @@ public abstract class WorkSchedulerTest extends TestCase {
 		private class Work implements WorkTarget<Integer> {
 			public int x = 10;
 			public synchronized Integer call() {
-				X.sayet("derp "+x);
 				x--;
+				$log.trace("reached count "+x);
 				return x;
 			}
 			public synchronized boolean isReady() {
@@ -216,10 +252,95 @@ public abstract class WorkSchedulerTest extends TestCase {
 	
 	
 	
-	/** Same as {@link TestNonblockingLeaderFollower}, but with many more WorkTargets than threads. */
-	private class TestNbWtMany extends TestNonblockingLeaderFollower {
+	/** Similar to {@link TestNonblockingLeaderFollower}, but with many more WorkTargets than threads, and all drawing from the same input pipe (which will have been closed before the draining WorkTargets start).
+	 * 
+	 * This is a very major test, since it deals with WorkTarget who have to notice their doneness and finish concurrently instead of at the completion of a normal run (probably).
+	 * 
+	 * This also tests (if indirectly) consistent results from WorkTarget that receive concurrent finishes (but I'd recommend running it numerous times if you want to feel confident of that).
+	 */
+	private class TestNonblockingManyWorkSingleSource extends TestCase.Unit {
+		protected WorkScheduler $ws = makeScheduler();
+		public final int HIGH = 10000;
+		public final int WTC = 32;
+		
+		private final Pipe<Integer> $pipe = new Pipe<Integer>();
+		protected final Work[] $wt = new Work[WTC];
+		@SuppressWarnings("unchecked")	// impossible to not suck in java.
+		protected final WorkFuture<Integer>[] $wf = Arr.newInstance(WorkFuture.class, WTC);
+		
 		public Object call() {
-			// ...some super.call() crap, but i really need a futurepipe to do this sensibly.
+			feedPipe();
+			
+			for (int $i = 0; $i < WTC; $i++)
+				$wt[$i] = new Work();
+			for (int $i = 0; $i < WTC; $i++)
+				$wf[$i] = $ws.schedule($wt[$i], ScheduleParams.NOW);
+			
+
+			try {
+				boolean $wonOnce = false;
+				for (int $i = 0; $i < WTC; $i++) {
+					int $ans = $wf[$i].get();
+					if ($ans == HIGH)
+						if ($wonOnce)
+							throw new AssertionFailed("More than one WorkTarget finished with the high value.");
+						else $wonOnce = true;
+				}
+				if (!$wonOnce) throw new AssertionFailed("No WorkTarget finished with the high value.");
+			}
+			catch (InterruptedException $e) { throw new AssertionFailed($e); }
+			catch (ExecutionException $e) { throw new AssertionFailed($e); }
+			
+			return null;
+		}
+		private class Work implements WorkTarget<Integer> {
+			public synchronized Integer call() {
+				return $pipe.SRC.readNow();
+			}
+			public synchronized boolean isReady() {
+				return !isDone();
+			}
+			public synchronized boolean isDone() {
+				return $pipe.SRC.hasNext();
+			}
+			public int getPriority() {
+				return 0;
+			}
+		}
+		protected void feedPipe() {
+			for (int $i = 0; $i < HIGH; $i++)
+				$pipe.SINK.write($i);
+			$pipe.SINK.close();
+		}
+	}
+	
+	
+	
+	/** Same as {@link TestNonblockingManyWorkSingleSource}, but the input pipe will be closed from the sink thread when the source is already empty (resulting in a (probably) concurrent finish for the WorkTargets). */
+	private class TestConcurrentFinish extends TestNonblockingManyWorkSingleSource {
+		public Object call() {
+			// the earlier test didn't actually need to set the pipe listener because all the writes were done before any reading started, and so all of the work was always ready as long as it wasn't done.  now we're in an entirely different situation.
+//			$pipe.SRC.setListener(new Listener<ReadHead<Integer>>() {
+//				public void hear(ReadHead<Integer> $rh) {
+//					
+//				}
+//			});
+			
+			breakIfFailed();
+			return null;
+		}
+		
+		protected void feedPipe() {
+			
+		}
+	}
+	
+	
+	
+	/**  */
+	private class TestPrioritizedDuo extends TestCase.Unit {
+		public Object call() {
+			breakIfFailed();
 			return null;
 		}
 	}
