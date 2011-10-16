ea4ea31b26989090be5e0ea2191d81ba7dc3e8ab
hash <hash@exultant.us>
2011-10-15 20:42:31 -0700

    That test program was a bit obtuse-looking, so I added a few quick inline comments about which lines are printing which lines.

--- src/main/us/exultant/ahs/terminal/exe/Test.java
+++ src/main/us/exultant/ahs/terminal/exe/Test.java
@@ -12,22 +12,26 @@ public class Test {
 		final int $h = T.getHeight();
 		
 		
+		// print top line, listing the background colors
 		T.print(Strings.padLeftToWidth(""," ",13));
 		for (Color $bg : Color.values()) {
 			T.setPalette(new Palette(Color.CLEAR, $bg));
 			T.print(Strings.padRightToWidth("  bg: "+$bg+" ",14));
 		}
+		
 		T.cursor().lineNext(1);
 		for (Color $fg : Color.values()) {
+			// print front of line, listing the forground color
 			T.setPalette(new Palette($fg, Color.CLEAR));
 			T.print(Strings.padLeftToWidth(" fg: "+$fg+" ",13));
+			
+			// print the table body
 			for (Color $bg : Color.values()) {
 				T.setPalette(new Palette($fg, $bg));
 				T.print("     word     ");
 			}
 			T.cursor().lineNext(1);
 		}
-		
 		
 		
 		Reader $r = System.console().reader();
