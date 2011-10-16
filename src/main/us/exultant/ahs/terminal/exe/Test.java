5be872692adfd4eda03ddbb5b6fef8941cc80b18
hash <hash@exultant.us>
2011-10-15 20:39:02 -0700

    Wrote up a quick little test of background and foreground coloring with the Palette system.
    (I decided to put this in the main tree of the source instead of the test tree because I can see folks actually wanting to run this to see if their system works, even if they're not even remotely development inclined.  I'm also starting a habit from now on of putting classes with entry points (i.e. main methods) in "exe" packages.)

---
+++ src/main/us/exultant/ahs/terminal/exe/Test.java
@@ -0,0 +1,48 @@
+package us.exultant.ahs.terminal.exe;
+
+import us.exultant.ahs.util.*;
+import us.exultant.ahs.terminal.*;
+import us.exultant.ahs.terminal.Terminal.Color;
+import java.io.*;
+
+public class Test {
+	private static final Terminal T = StandardTerminal.get();
+	public static void main(String... args) {
+		final int $w = T.getWidth();
+		final int $h = T.getHeight();
+		
+		
+		T.print(Strings.padLeftToWidth(""," ",13));
+		for (Color $bg : Color.values()) {
+			T.setPalette(new Palette(Color.CLEAR, $bg));
+			T.print(Strings.padRightToWidth("  bg: "+$bg+" ",14));
+		}
+		T.cursor().lineNext(1);
+		for (Color $fg : Color.values()) {
+			T.setPalette(new Palette($fg, Color.CLEAR));
+			T.print(Strings.padLeftToWidth(" fg: "+$fg+" ",13));
+			for (Color $bg : Color.values()) {
+				T.setPalette(new Palette($fg, $bg));
+				T.print("     word     ");
+			}
+			T.cursor().lineNext(1);
+		}
+		
+		
+		
+		Reader $r = System.console().reader();
+		char[] $cbuf = new char[1];
+		try {
+			while (true) {
+				if ($r.ready()) {
+					$r.read($cbuf);
+					System.console().printf(new String($cbuf));
+				}
+			}
+		} catch (IOException $e) {
+			$e.printStackTrace();
+		}
+		
+		StandardTerminal.get().clear();
+	}
+}
