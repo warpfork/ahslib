24723a285ace48c36e3fa361d4cd1d3cd39f2c98
hash <hash@exultant.us>
2011-11-01 01:08:38 -0700

    Improved the schedule method on WorkSchedulerFlexiblePriority to correctly detect tasks that are finished either during or before their submission and fire their finish events.

--- src/main/us/exultant/ahs/thread/WorkSchedulerFlexiblePriority.java
+++ src/main/us/exultant/ahs/thread/WorkSchedulerFlexiblePriority.java
@@ -43,18 +43,24 @@ public class WorkSchedulerFlexiblePriority implements WorkScheduler {
 	
 	public <$V> WorkFuture<$V> schedule(WorkTarget<$V> $work, ScheduleParams $when) {
 		WorkFuture<$V> $wf = new WorkFuture<$V>($work, $when);
-			//FIXME:AHS:THREAD: this next line won't actually notice if the task that was submitted was already finished (it won't schedule it either, which is good, but not finishing it is still a bug (we wouldn't want someone to wait on the future if it'll never transition to finished, right?)).  (the RelentlessGC will deal with it eventually of course, but it's much better if we notice it immediately -- then we don't even have to lock the scheduler to insert the new task.
 		$lock.lock();
 		try {
-			if ($wf.$sync.scheduler_shift())
+			if ($wf.$sync.scheduler_shift()) {
 				$scheduled.add($wf);
-			else
+			} else {
 				if ($when.isUnclocked()) {
 					$unready.add($wf);
 //					$log.trace(this, "frosh to unready: "+$wf);
-				}
-				else
+				} else {
 					$delayed.add($wf);
+				}
+
+				if ($work.isDone()) {
+					// this check MUST be done AFTER adding the task to a heap, or otherwise it becomes more than slightly dodgy, for all the usual reasons: you could have failed to shift because you just weren't ready, then the done check happens, then you concurrently "finish" from someone else draining your pipe before this scheduler knows to take update requests about you seriously.
+					$wf.$sync.tryFinish(false, null, null);
+					hearTaskDrop($wf);
+				}
+			}
 			return $wf;
 		} finally {
 			$lock.unlock();
