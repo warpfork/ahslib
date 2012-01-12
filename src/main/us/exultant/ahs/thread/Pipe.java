3e5ea5b2b32d9e098e50ee99c818cf133ba7c5be
hash <hash@exultant.us>
2012-01-12 02:27:32 -0800

    Add a sanity check to Pipe.
    Whether or not this will hang around for long is unsure.

--- src/main/us/exultant/ahs/thread/Pipe.java
+++ src/main/us/exultant/ahs/thread/Pipe.java
@@ -285,7 +285,8 @@ public class Pipe<$T> implements Flow<$T> {
 			try {
 				if (isClosed()) throw new IllegalStateException("Pipe has been closed.");
 				$queue.add($chunk);
-				$gate.release();	// this can't return false because gate closure involves locking, and we're locked right now.  and that's essentially why the write-lock exists (otherwise we'd have to go dredge back through the queue if this did return false, and that would be... mucky).
+				boolean $true = $gate.release();	// this can't return false because gate closure involves locking, and we're locked right now.  and that's essentially why the write-lock exists (otherwise we'd have to go dredge back through the queue if this did return false, and that would be... mucky).
+				if (!$true) throw new MajorBug("this isn't supposed to be possible.");
 				
 				Listener<ReadHead<$T>> $el_dated = $el;
 				if ($el_dated != null) $el_dated.hear(SRC);
