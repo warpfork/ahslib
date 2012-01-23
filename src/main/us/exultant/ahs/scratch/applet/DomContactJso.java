439a38a7537d28bceb263466dda01246eff5581d
hash <hash@exultant.us>
2012-01-22 18:38:07 -0800

    Read some books about java/javascript interaction and formed some javadocs about things that were previously not specified in DomContactJso.

--- src/main/us/exultant/ahs/scratch/applet/DomContactJso.java
+++ src/main/us/exultant/ahs/scratch/applet/DomContactJso.java
@@ -22,6 +22,12 @@ package us.exultant.ahs.scratch.applet;
 import java.applet.*;
 import netscape.javascript.*;
 
+/**
+ * Allows an applet to interact with the DOM it is embedded in via javascript.
+ * 
+ * @author hash
+ * 
+ */
 public class DomContactJso implements DomContact.Exposure {
 	public DomContactJso(Applet $applet) {
 		$jso = JSObject.getWindow($applet);
@@ -31,6 +37,30 @@ public class DomContactJso implements DomContact.Exposure {
 	private JSObject	$jso;
 	private StringBuffer	$precommand;
 	
+	/**
+	 * <p>
+	 * Executes a javascript string in the context of the browser window containing
+	 * our applet.
+	 * </p>
+	 * 
+	 * <p>
+	 * Note about return types: they're... very ambiguous and not at all convenient.
+	 * The author recommends giving up using anything except strings; if you need full
+	 * objects or arrays (or even simple things like integers), pass them around as
+	 * JSON strings and deal with encoding and decoding on either side.
+	 * </p>
+	 * 
+	 * @see <a
+	 *      href="http://docstore.mik.ua/orelly/web/jscript/refp_186.html">Javascript
+	 *      - The Definitive Guide</a>
+	 * 
+	 * @param $strs
+	 *                a String or set of Strings which will be concatenated and run as
+	 *                javascript.
+	 * @return this may return a {@link JSObject}, {@link Boolean}, {@link Double}, or
+	 *         {@link String}. If multiple javascript commands were in the strings we
+	 *         were given to run, the value returned is from the last of them.
+	 */
 	public synchronized Object eval(String... $strs) {
 		if ($strs.length == 0) return null;
 		if ($strs.length == 1) return $jso.eval($strs[0]);
