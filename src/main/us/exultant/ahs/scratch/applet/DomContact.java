364ca54bcd632175ef598473f598be5e6636ac70
hash <hash@exultant.us>
2012-01-22 23:34:08 -0800

    Scooped out at lot of the old applet stuff.  I was over-engineering, and not well.  (And if there's any place in the world it's wildly unrealistic to try to enforce any kind of sanity, it's when touching javascript.)

--- src/main/us/exultant/ahs/scratch/applet/DomContact.java
+++ src/main/us/exultant/ahs/scratch/applet/DomContact.java
@@ -19,62 +19,115 @@
 
 package us.exultant.ahs.scratch.applet;
 
-import us.exultant.ahs.scratch.applet.command.*;
+import java.util.*;
+import java.util.regex.*;
 
+/**
+ * <p>
+ * Allows an applet to interact with the DOM it is embedded in via javascript.
+ * </p>
+ * 
+ * <p>
+ * Several pieces of groundwork are performed when this is initialized in order to make
+ * the browser easier to deal with and make some shorthand functions globally available:
+ * <ul>
+ * <li>the ID of the first Body tag is set to "body"
+ * <li>the ID of the first Head tag is set to "head"
+ * <li>creates a div called "main" and adds it to id:body
+ * <li>creates a div called "dev" and adds it to id:body (and sets id:dev to invisible
+ * before adding it)
+ * <li>function {@code gebi} aliases {@code document.getElementById}
+ * <li>function {@code gebt} aliases {@code document.getElementsByTagName}
+ * <li>function {@code newe} aliases {@code document.createElement}
+ * <li>function {@code newt} aliases {@code document.createTextNode}
+ * <li>finally, attempts to set the focus to id:main.
+ * </ul>
+ * The element id:main should be used to contain all the rest of the page &mdash;
+ * modifying any elements of the DOM above that can be dangerous, because results are
+ * unpredictable if the applet itself is unlinked from the DOM. The element id:dev is
+ * intended for use as a debugging output area.
+ * </p>
+ * 
+ * @author hash
+ * 
+ */
 public class DomContact {
 	public DomContact(Exposure $exposure) {
-		$power = $exposure;
+		this.$power = $exposure;
+		$power.eval("dS = document.createElement('script'); dS.type = 'text/javascript'; dS.innerHTML = \""+CRIT+"\"; document.getElementsByTagName('head')[0].appendChild(dS);");
+		$power.eval("gebt('head')[0].id = 'head';");
+		$power.eval("gebt('body')[0].id = 'body';");
+		$power.eval("dB = newe('div'); dB.id = 'main'; gebi('body').appendChild(dB);");
+		$power.eval("dB = newe('div'); dB.id = 'dev'; dB.style.visibility = 'hidden'; gebi('body').appendChild(dB);");
+		$power.eval("gebi('main').focus()");
 	}
+	private static final String CRIT =	"function gebi($id)  {	return document.getElementById($id);		}	\\n" + 
+						"function gebt($tag) {	return document.getElementsByTagName($tag);	}	\\n" + 
+						"function newe($tag) {	return document.createElement($tag);		}	\\n" + 
+						"function newt($txt) {	return document.createTextNode($txt);		}	\\n" + 
+						"\\n" +
+						"function removeElement(element) {						\\n" + 
+						"	if (!element) return false;						\\n" + 
+						"	element.parentNode.removeChild(element);				\\n" + 
+						"	delete element;								\\n" + 
+						"	return true;								\\n" + 
+						"}										\\n" + 
+						"" + 
+						"";
+	
+	private final Exposure	$power;
 	
 	/**
 	 * <p>
-	 * Prepares the JsCommand for execution by providing it with direct exposure to
-	 * the javascript evaluator, then runs it.
-	 * </p>
-	 * 
-	 * <p>
-	 * Typical usage of DomContact might look something like this:
-	 * 
-	 * <pre>
-	 * DomContact $domContact = new DomContact(new DomContactJso($applet));
-	 * ... 
-	 * 
-	 * JsCommand&lt;Bees&gt; $jsc = new FuckWitThisPropCmd(...);
-	 * $domContact.execute($jsc);
-	 * Bees $answer = $jsc.get();
-	 * </pre>
-	 * 
-	 * </p>
-	 * 
-	 * <p>
-	 * Be aware that this method is NOT synchronized. Individual DOM accesses via an
-	 * Exposure should always be atomic, but when two JsCommand are run simultaneously
-	 * it is not guaranteed which order they will recieve answers from the Exposure,
-	 * and if they make multiple calls to the Exposure or chain invocations to other
-	 * JsCommand, there is no guarantee of order or atomicity until they enforce it by
-	 * explicitly synchronizing on the Exposure object themselves.
+	 * Executes a javascript string in the context of the browser window containing
+	 * our applet.
 	 * </p>
 	 */
-	public void execute(JsCommand<?> $jsc) {
-		$jsc.run($power);
+	public Object eval(String... $strs) {
+		return $power.eval($strs);
 	}
 	
-	
-	
-	private Exposure $power;
+	/**
+	 * Convenience method for setting attributes on objects (useful only for DOM
+	 * objects that already have IDs assigned and for values that are strings).
+	 */
+	public void setAttribute(String $domObjId, String $attrib, String $value) {
+		$power.eval("gebi(\"", $domObjId, "\").", $attrib, "=\"", $value, "\";");
+	}
 	
 	/**
-	 * Every DomContact implementor should also have a nested class that implements
-	 * Exposure. It is recommended that a singleton instance of Exposure be
-	 * maintained, and be private to the DomContact. The DomContact can then give a
-	 * pointer to the Exposure to JsCommands during their execution; this prevents
-	 * leakage of references to the Exposure instance except when a JsCommand chains
-	 * invocations to other JsCommands (which is fine) or when a JsCommand keeps a
-	 * pointer to the Exposure and returns it later (which is... inadvisable and ought
-	 * not be done).
-	 */// i might be doing a noob here.  Exposure might be the only thing that needs to be an interface; after that DomContact can pretty much be a final class.  we'll see.
+	 * Convenience method for getting attributes on objects (useful only for DOM
+	 * objects that already have IDs assigned and for values that are strings).
+	 */
+	public String getAttribute(String $domObjId, String $attrib) throws Exception {
+		return $power.eval("gebi(", $domObjId, ").", $attrib, "\";").toString();
+	}
+	
+	/**
+	 * Grabs the "window location" from a browser window and parses the GET part of
+	 * the it to produce a Map<String,String> that maps keys to values; if the GET
+	 * contains the same key multiple times, the map will contain the value set in the
+	 * last instance.
+	 */
+	public Map<String,String> getQueryGet() {
+		String $get = (String)$power.eval("window.location.search.substring(1)");
+		String[] $entries = $get.split(PAT_AMP);
+		Map<String,String> $map = new HashMap<String,String>();
+		for (String $ent : $entries) {
+			String[] $split = $ent.split(PAT_EQ, 2);
+			if ($split.length == 1)
+				$map.put($split[0], "");
+			else
+				$map.put($split[0], $split[1]);
+		}
+		return $map;
+	}
+	private static final String PAT_AMP = Pattern.quote("&");
+	private static final String PAT_EQ = Pattern.quote("=");
+	
+	
+	
 	public static interface Exposure {
-		/** This method should perform its operation atomically. */
-		public Object eval(String... $strs);	// i still don't really know what's going on with this Object return type, to be honest, but it's what JSO does so i'm going with it until it bites me.
+		public Object eval(String... $strs);
 	}
 }
