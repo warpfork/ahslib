00e707b5c790c035e7096b9cdc773dbfd6c70483
hash <hash@exultant.us>
2012-02-08 01:54:52 -0800

    Made some minor changes to documentation formatting.

--- dox/index.php
+++ dox/index.php
@@ -1,26 +1,29 @@
-<?php require($_SERVER['DOCUMENT_ROOT']."/style/head.php"); ?>
-<?php echo head("exultant.us AHSlib"); ?>
-<?php echo theme(); ?>
-<link rel='stylesheet' href='style.css' type='text/css'>
-<html id='ahslib-dox'><body class='std'>
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/style/head.php");
+echo Theme::head(
+	"exultant.us software:AHSlib"
+);
+?>
 
-<h1 style='float:right; margin: 0.5em 8em;'>And How Simple!</h1>
 
-<p>
-	The "And How Simple!" library provides open-source Java solutions for rapid development of applications utilizing networking, serialization/persistence, concurrency control, and thread scheduling.  The primary goals of AHSlib are remove common drudgery from coding in java wherever possible, make writing multithreaded applications easier, and codify common design patterns so that good code becomes more self-documenting.  AHSlib  is tested for both stability and performance and comes complete with an abundance of abstract interfaces that made it sufficient to provide services to a diverse range of projects.
-</p>
-<p>
-	The AHS library is free and open source.  The codec modules are released under <a href=http://www.apache.org/licenses/LICENSE-2.0>Apache License 2.0</a>; all other modules of the library are released under <a href=http://www.gnu.org/licenses/lgpl.html>GNU LGPL v3</a>.  The AHS library contains some components derived from other open sources; see the code for their attached licenses.
-</p>
-<p>
-	AHSlib's primary source repository is held by Git and managed with Redmine &mdash; check it out: <a href='https://rubicon.exultant.us/redmine/projects/ahslib'>https://rubicon.exultant.us/redmine/projects/ahslib</a>
-</p>
-<p>
-	The javadocs are <a href='javadoc'>here</a>.  Introductory and overview docuemntation that's more forgiving to new users is coming soon.
-</p>
-<p>
-	This documentation is in the repository itself, so you can both read <i>and</i> contribute to it via git!
-</p>
+<div class="grid_14 prefix_1">
+	<h1 style='float:right; margin: 0.5em 8em;'>And How Simple!</h1>
+	
+	<p>
+		The "And How Simple!" library provides open-source Java solutions for rapid development of applications utilizing networking, serialization/persistence, concurrency control, and thread scheduling.  The primary goals of AHSlib are remove common drudgery from coding in java wherever possible, make writing multithreaded applications easier, and codify common design patterns so that good code becomes more self-documenting.  AHSlib  is tested for both stability and performance and comes complete with an abundance of abstract interfaces that made it sufficient to provide services to a diverse range of projects.
+	</p>
+	<p>
+		The AHS library is free and open source.  The codec modules are released under <a href=http://www.apache.org/licenses/LICENSE-2.0>Apache License 2.0</a>; all other modules of the library are released under <a href=http://www.gnu.org/licenses/lgpl.html>GNU LGPL v3</a>.  The AHS library contains some components derived from other open sources; see the code for their attached licenses.
+	</p>
+	<p>
+		AHSlib's primary source repository is held by Git and managed with Redmine &mdash; check it out: <a href='https://rubicon.exultant.us/redmine/projects/ahslib'>https://rubicon.exultant.us/redmine/projects/ahslib</a>
+	</p>
+	<p>
+		The javadocs are <a href='javadoc'>here</a>.  Introductory and overview docuemntation that's more forgiving to new users is coming soon.
+	</p>
+	<p>
+		This documentation is in the repository itself, so you can both read offline and contribute to it via git.
+	</p>
+</div>
 
-</body></html>
-<?php echo foot(); ?>
+
