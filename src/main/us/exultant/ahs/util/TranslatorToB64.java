b38bb3e315803e9c90bba7659b8e88ebf96f876f
hash <hash@exultant.us>
2012-02-18 16:58:04 -0800

    Created a set of Translator implementations for base64 and hex, and a test unit for translators.  (There's no functionality that's direly new here of course; just fitting interfaces for handier composition.)

---
+++ src/main/us/exultant/ahs/util/TranslatorToB64.java
@@ -0,0 +1,20 @@
+package us.exultant.ahs.util;
+
+import us.exultant.ahs.core.*;
+import java.nio.*;
+
+/** Like it says on the tin. */
+public class TranslatorToB64 implements Translator<ByteBuffer,String> {
+	public static final TranslatorToB64 INSTANCE = new TranslatorToB64(); 
+	
+	/**
+	 * @param $bytes
+	 *                a ByteBuffer to read between position and limit
+	 * @return a base64 string
+	 */
+	public final String translate(ByteBuffer $bytes) {
+		byte[] $x = new byte[$bytes.limit() - $bytes.position()];
+		$bytes.get($x);	//XXX:AHS:EFFIC: this copy is silly.  we should alter the Base64 class to allow offset+range operations and then make this better.
+		return Base64.encode($x);
+	}
+}
---
+++ src/test/us/exultant/ahs/util/TestTranslators.java
@@ -0,0 +1,39 @@
+package us.exultant.ahs.util;
+
+import us.exultant.ahs.core.*;
+import us.exultant.ahs.log.*;
+import us.exultant.ahs.test.*;
+import java.nio.*;
+import java.util.*;
+
+public class TestTranslators extends TestCase {
+	public static void main(String... $args) {				new TestTranslators().run();			}
+	public TestTranslators() {						super(new Logger(Logger.LEVEL_TRACE), true);	}
+	public TestTranslators(Logger $log, boolean $enableConfirmation) {	super($log, $enableConfirmation);		}
+	public List<Unit> getUnits() {
+		return Arr.<Unit>asList(
+				new TestHexConsistency(),
+				new TestHexAccuracy()
+		);
+	}
+
+	/**  */
+	private class TestHexConsistency extends TestCase.Unit {
+		public Object call() throws TranslationException {
+			byte[] $x = new byte[] { (byte)0xCA, (byte)0xFE, (byte)0xBA, (byte)0xBE };
+			byte[] $y = TranslatorFromHex.INSTANCE.translate(TranslatorToHex.INSTANCE.translate(ByteBuffer.wrap($x))).array();
+			assertEquals($x, $y);
+			return null;
+		}
+	}
+	
+	/**  */
+	private class TestHexAccuracy extends TestCase.Unit {
+		public Object call() throws TranslationException {
+			byte[] $x = new byte[] { (byte)0xCA, (byte)0xFE, (byte)0xBA, (byte)0xBE };
+			byte[] $y = TranslatorFromHex.INSTANCE.translate("CAFEBABE").array();
+			assertEquals($x, $y);
+			return null;
+		}
+	}
+}
