040cd9c7914266ea38477eccd885ae17ec05aa8a
hash <hash@exultant.us>
2012-04-22 10:11:21 -0700

    Refactoring SelectionSignaller; the parts of that system that implement WorkTarger are now concealled and rather than being messily and distractingly exposed from the same type as the methods for interest registrations.  Also, creating IOManager -- it's the same role as WorkManager was for the threading package; static facades, singleton holders, that stuff.  It's holding a lazy singleton for a SelectionSignaller, of course.

--- src/main/us/exultant/ahs/io/SelectionSignaller.java
+++ src/main/us/exultant/ahs/io/SelectionSignaller.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2010, 2011 Eric Myhre <http://exultant.us>
+ * 
+ * This file is part of AHSlib.
+ *
+ * AHSlib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License, or
+ * (at the original copyright holder's option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package us.exultant.ahs.io;
 
 import us.exultant.ahs.core.*;
@@ -21,16 +40,26 @@ import java.util.*;
  * </p>
  * 
  * <p>
- * Note that this WorkTarget is not so ammenable to pooling as one might like;
- * unfortunately, there were design choices made in the core of the Java standard
- * libraries that leave us with a lot of limitations. In particular, we have no way of
- * getting events that the selector itself has events ready other than full-out checking
- * it &mdash; so, we're stuck with an {@link #isReady()} method that helplessly always
- * returns true, and fundamentally no way to disbatch events relating to the core
- * selector's readiness. So, all in all, you may still actually need to just run this
- * system in its own personal thread (creating a
+ * There is a default global singleton instance of this available from
+ * {@link IOManager#getDefaultSelectionSignaller()} that should be used in almost every
+ * situation.
+ * </p>
+ * 
+ * <p>
+ * If you choose not to use the default global instance of this selector and instead
+ * construct and schedule your own, please note that this WorkTarget is not so ammenable
+ * to pooling as one might like; unfortunately, there were design choices made in the core
+ * of the Java standard libraries that leave us with a lot of limitations. In particular,
+ * we have no way of getting events that the selector itself has events ready other than
+ * full-out checking it &mdash; so, we're stuck with an {@link Worker#isReady()} method
+ * that helplessly always returns true, and fundamentally no way to disbatch events
+ * relating to the core selector's readiness. There are two ways to deal with this: you
+ * may actually just run this system in its own personal thread (creating a
  * {@link WorkSchedulerFlexiblePriority} instance with a thread pool of size one is a
- * reasonable way to do this).
+ * reasonable way to do this); otherwise, if you do wish to keep it in the same Scheduler
+ * as other tasks, you should be sure to construct this selector with purely nonblocking
+ * mode (negative selection timeout parameter) and fixed-delay scheduling parameters (i.e.
+ * {@link ScheduleParams#makeFixedDelay(long)}).
  * </p>
  * 
  * @author Eric Myhre <tt>hash@exultant.us</tt>
@@ -38,7 +67,7 @@ import java.util.*;
  */
 // in a perfect world, i'd like to be able to treat connection acceptance or whathaveyou has a separate priority than readability or whathaveyou.
 //   unfortunately the only way i can see to do that is by having completely separate selectors and taking it upon yourself to register things to them by category.  and eat your moar threads.
-public class SelectionSignaller implements WorkTarget<Void> {
+public class SelectionSignaller {
 	/**
 	 * Creates a new system default Selector back-end. Selects run with a timeout of 1
 	 * millisecond; the WorkTarget's priority is zero.
@@ -81,7 +110,7 @@ public class SelectionSignaller implements WorkTarget<Void> {
 	 * @param $workPriority
 	 */
 	public SelectionSignaller(int $selectionTimeout, int $workPriority) {
-		this(makeDefaultSelector(), $selectionTimeout, 0);
+		this(makeDefaultSelector(), $selectionTimeout, $workPriority);
 	}
 	
 	private static Selector makeDefaultSelector() {
@@ -108,136 +137,14 @@ public class SelectionSignaller implements WorkTarget<Void> {
 		});
 	}
 	
-	private final Selector	$selector;
-	private final int	$priority;
-	private final int	$timeout;
-	
-	/**
-	 * This always returns true. I'm sorry. The core java api for {@link Selector}
-	 * leaves me no choice &mdash; it provides no method for inquiring about readiness
-	 * without actually performing a select.
-	 */
-	public boolean isReady() {
-		return true;
-		//try {
-		//	return 
-		//		$selector.selectedKeys().isEmpty() ||		// i don't think this works, actually, because you have to call select() in order to get that key set to be updated by the OS.  Also: mind that if you DID use selectNow here, you'd have to change the $freshWorkExists boolean in the call method, because that optimization would no longer be valid. 
-		//		$pipe.hasNext();
-		//} catch (ClosedSelectorException $e) {
-		//	return false;
-		//}
+	public WorkFuture<Void> schedule(WorkScheduler $scheduler, ScheduleParams $when) {
+		return $scheduler.schedule(new Worker(), $when);
 	}
 	
-	public boolean isDone() {
-		return !$selector.isOpen();
-	}
-	
-	public int getPriority() {
-		return $priority;
-	}
-	
-	/**
-	 * <p>
-	 * This method cycles through three phases. In the first phase it blocks on the
-	 * selector. In the second phase it calls the listeners for every channel that
-	 * reported an event. In the third phase, the internal event buffer used to
-	 * synchronize registration and deregistration requests from other threads is
-	 * checked, and any buffered events are processed.
-	 * </p>
-	 */
-	public Void call() {
-		// PHASE ONE
-		// check for new registration or deregistration events
-		callRegistrationProcessing();
-		
-		// PHASE TWO
-		// chill out
-		boolean $freshWorkExists = callSelect() > 0;
-		
-		// PHASE... TWO AND A HALF?
-		// if we were a blocking selector, we might have been woken up specifically to deal with a new event, so we should do so asap
-		//callRegistrationProcessing();
-		// actually no, this is silly.  if we're being used in blocking mode, we're probably also being used in our own thread, so we'll be looping back to phase one momentarily anyway and it's all kay.
-		
-		// PHASE THREE
-		// disbatch events to folks who're deserving
-		if ($freshWorkExists) callDisbatchEvents();
-		
-		return null;
-	}
-	
-	private int callSelect() {
-		try {
-			/* block until channel events, or wakeups triggered by the event pipe's listener, or thread interrupts. */
-			if ($timeout < 0) return $selector.selectNow();
-			return $selector.select($timeout);
-		} catch (ClosedSelectorException $e) {
-			/* selectors can't be closed except by their close method, which we control all access to, so this shouldn't happen in a way that surprises us. */
-			throw new MajorBug($e);
-		} catch (IOException $e) {
-			/* I just plain don't know what would cause this. */
-			throw new Error($e);
-		}
-	}
-	
-	private void callDisbatchEvents() {
-		Iterator<SelectionKey> $itr = $selector.selectedKeys().iterator();
-		while ($itr.hasNext()) {
-			SelectionKey $k = $itr.next();
-			$itr.remove();
-			
-			if (!$k.isValid()) continue;
-			
-			int $ops = $k.readyOps();
-			Attache $a = (Attache) $k.attachment();
-			if (Primitives.containsFullMask($ops, SelectionKey.OP_CONNECT)) try { if (((SocketChannel)$k.channel()).finishConnect()) $k.interestOps(Primitives.removeMask($k.interestOps(), SelectionKey.OP_CONNECT)); } catch (IOException $e) { $k.cancel(); }
-			if (Primitives.containsFullMask($ops, SelectionKey.OP_READ) && $a.$reader != null) $a.$reader.hear($k.channel());
-			if (Primitives.containsFullMask($ops, SelectionKey.OP_WRITE) && $a.$writer != null) $a.$writer.hear($k.channel());
-			if (Primitives.containsFullMask($ops, SelectionKey.OP_ACCEPT) && $a.$accepter != null) $a.$accepter.hear($k.channel());
-		}
-	}
-	
-	private void callRegistrationProcessing() {
-		List<Event> $evts = $pipe.source().readAllNow();
-		for (Event $evt : $evts) {
-			if ($evt instanceof Event_Reg) {
-				SelectionKey $k = $evt.$chan.keyFor($selector);
-				Attache $a;
-				int $old_ops;
-				if ($k == null) {
-					$a = new Attache();
-					$old_ops = 0;
-				} else {
-					$a = (Attache) $k.attachment();
-					$old_ops = $k.interestOps();
-				}
-				$a.apply($evt);
-				// there miiiiight be some potential optimizations here for detecting when we don't actually need to change the op set.  in fact i'd say there definitely are.  well, on third thought, dunno, it might not be that big of a deal as long as we're assuming that the channel doesn't have tons of selectable keys, because deep down it's O(n)'ing on that, not the selector's whole key set.
-				try {
-					$evt.$chan.register($selector, Primitives.addMask($old_ops, $evt.$ops), $a);
-				} catch (ClosedChannelException $e) {
-					/* there's nothing we can really do to "recover" here; we just... don't do anything because there's nothing to do. */
-				}
-			} else if ($evt instanceof Event_Dereg) {
-				SelectionKey $k = getKey($evt);
-				$k.interestOps(Primitives.removeMask($k.interestOps(), $evt.$ops));
-				Attache $a = (Attache) $k.attachment();
-				if (Primitives.containsFullMask($evt.$ops, SelectionKey.OP_READ) && $a.$reader != null) $a.$reader = null; // gc help
-				if (Primitives.containsFullMask($evt.$ops, SelectionKey.OP_WRITE) && $a.$writer != null) $a.$writer = null; // gc help
-				if (Primitives.containsFullMask($evt.$ops, SelectionKey.OP_ACCEPT) && $a.$accepter != null) $a.$accepter = null; // gc help
-			} else if ($evt instanceof Event_Cancel) {
-				SelectionKey $k = getKey($evt);
-				if ($k != null) $k.cancel();
-				$k.attach(null); // gc help
-			}
-		}
-	}
-	
-	
-	
-	
-	
+	private final Selector		$selector;
 	private final Pipe<Event>	$pipe;
+	private final int		$priority;
+	private final int		$timeout;
 	
 	private SelectionKey getKey(Event $evt) {
 		if ($evt.$chan != null) return $evt.$chan.keyFor($selector);
@@ -247,34 +154,6 @@ public class SelectionSignaller implements WorkTarget<Void> {
 			if ($a.contains($evt.$listener)) return $k;
 		}
 		return null;
-	}
-	
-	
-	
-	private class Attache {
-		public Listener<SelectableChannel>	$reader;
-		public Listener<SelectableChannel>	$writer;
-		public Listener<SelectableChannel>	$accepter;
-		
-		public void apply(Event $evt) {
-			switch ($evt.$ops) {
-				case SelectionKey.OP_READ:
-					$reader = $evt.$listener;
-					break;
-				case SelectionKey.OP_WRITE:
-					$writer = $evt.$listener;
-					break;
-				case SelectionKey.OP_ACCEPT:
-					$accepter = $evt.$listener;
-					break;
-				default:
-					throw new MajorBug("op type not supported");
-			}
-		}
-		
-		public boolean contains(Listener<SelectableChannel> $p) {
-			return ($reader == $p) || ($writer == $p) || ($accepter == $p);
-		}
 	}
 	
 	/**
@@ -414,18 +293,12 @@ public class SelectionSignaller implements WorkTarget<Void> {
 		public final Listener<SelectableChannel>	$listener;
 		public final int				$ops;
 	}
-	
-	
-	
 	private static class Event_Reg extends Event {
 		private Event_Reg(SelectableChannel $ch, Listener<SelectableChannel> $p, int $ops) {
 			super($ch, $p, $ops);
 			if ($p == null) throw new NullPointerException("listener cannot be null");
 		}
 	}
-	
-	
-	
 	private static class Event_Dereg extends Event {
 		private Event_Dereg(Listener<SelectableChannel> $p, int $ops) {
 			super(null, $p, $ops);
@@ -435,9 +308,6 @@ public class SelectionSignaller implements WorkTarget<Void> {
 			super($ch, null, $ops);
 		}
 	}
-	
-	
-	
 	private static class Event_Cancel extends Event {
 		private Event_Cancel(SelectableChannel $ch) {
 			super($ch, null, 0);
@@ -445,6 +315,159 @@ public class SelectionSignaller implements WorkTarget<Void> {
 		
 		private Event_Cancel(Listener<SelectableChannel> $p) {
 			super(null, $p, 0);
+		}
+	}
+	
+	
+	
+	private class Worker implements WorkTarget<Void> {
+		/**
+		 * This always returns true. I'm sorry. The core java api for {@link Selector}
+		 * leaves me no choice &mdash; it provides no method for inquiring about readiness
+		 * without actually performing a select.
+		 */
+		public boolean isReady() {
+			return true;
+			//try {
+			//	return 
+			//		$selector.selectedKeys().isEmpty() ||		// i don't think this works, actually, because you have to call select() in order to get that key set to be updated by the OS.  Also: mind that if you DID use selectNow here, you'd have to change the $freshWorkExists boolean in the call method, because that optimization would no longer be valid. 
+			//		$pipe.hasNext();
+			//} catch (ClosedSelectorException $e) {
+			//	return false;
+			//}
+		}
+		
+		public boolean isDone() {
+			return !$selector.isOpen();
+		}
+		
+		public int getPriority() {
+			return $priority;
+		}
+		
+		/**
+		 * <p>
+		 * This method cycles through three phases. In the first phase it blocks on the
+		 * selector. In the second phase it calls the listeners for every channel that
+		 * reported an event. In the third phase, the internal event buffer used to
+		 * synchronize registration and deregistration requests from other threads is
+		 * checked, and any buffered events are processed.
+		 * </p>
+		 */
+		public Void call() {
+			// PHASE ONE
+			// check for new registration or deregistration events
+			callRegistrationProcessing();
+			
+			// PHASE TWO
+			// chill out
+			boolean $freshWorkExists = callSelect() > 0;
+			
+			// PHASE... TWO AND A HALF?
+			// if we were a blocking selector, we might have been woken up specifically to deal with a new event, so we should do so asap
+			//callRegistrationProcessing();
+			// actually no, this is silly.  if we're being used in blocking mode, we're probably also being used in our own thread, so we'll be looping back to phase one momentarily anyway and it's all kay.
+			
+			// PHASE THREE
+			// disbatch events to folks who're deserving
+			if ($freshWorkExists) callDisbatchEvents();
+			
+			return null;
+		}
+		
+		private int callSelect() {
+			try {
+				/* block until channel events, or wakeups triggered by the event pipe's listener, or thread interrupts. */
+				if ($timeout < 0) return $selector.selectNow();
+				return $selector.select($timeout);
+			} catch (ClosedSelectorException $e) {
+				/* selectors can't be closed except by their close method, which we control all access to, so this shouldn't happen in a way that surprises us. */
+				throw new MajorBug($e);
+			} catch (IOException $e) {
+				/* I just plain don't know what would cause this. */
+				throw new Error($e);
+			}
+		}
+		
+		private void callDisbatchEvents() {
+			Iterator<SelectionKey> $itr = $selector.selectedKeys().iterator();
+			while ($itr.hasNext()) {
+				SelectionKey $k = $itr.next();
+				$itr.remove();
+				
+				if (!$k.isValid()) continue;
+				
+				int $ops = $k.readyOps();
+				Attache $a = (Attache) $k.attachment();
+				if (Primitives.containsFullMask($ops, SelectionKey.OP_CONNECT)) try { if (((SocketChannel)$k.channel()).finishConnect()) $k.interestOps(Primitives.removeMask($k.interestOps(), SelectionKey.OP_CONNECT)); } catch (IOException $e) { $k.cancel(); }
+				if (Primitives.containsFullMask($ops, SelectionKey.OP_READ) && $a.$reader != null) $a.$reader.hear($k.channel());
+				if (Primitives.containsFullMask($ops, SelectionKey.OP_WRITE) && $a.$writer != null) $a.$writer.hear($k.channel());
+				if (Primitives.containsFullMask($ops, SelectionKey.OP_ACCEPT) && $a.$accepter != null) $a.$accepter.hear($k.channel());
+			}
+		}
+		
+		private void callRegistrationProcessing() {
+			List<Event> $evts = $pipe.source().readAllNow();
+			for (Event $evt : $evts) {
+				if ($evt instanceof Event_Reg) {
+					SelectionKey $k = $evt.$chan.keyFor($selector);
+					Attache $a;
+					int $old_ops;
+					if ($k == null) {
+						$a = new Attache();
+						$old_ops = 0;
+					} else {
+						$a = (Attache) $k.attachment();
+						$old_ops = $k.interestOps();
+					}
+					$a.apply($evt);
+					// there miiiiight be some potential optimizations here for detecting when we don't actually need to change the op set.  in fact i'd say there definitely are.  well, on third thought, dunno, it might not be that big of a deal as long as we're assuming that the channel doesn't have tons of selectable keys, because deep down it's O(n)'ing on that, not the selector's whole key set.
+					try {
+						$evt.$chan.register($selector, Primitives.addMask($old_ops, $evt.$ops), $a);
+					} catch (ClosedChannelException $e) {
+						/* there's nothing we can really do to "recover" here; we just... don't do anything because there's nothing to do. */
+					}
+				} else if ($evt instanceof Event_Dereg) {
+					SelectionKey $k = getKey($evt);
+					$k.interestOps(Primitives.removeMask($k.interestOps(), $evt.$ops));
+					Attache $a = (Attache) $k.attachment();
+					if (Primitives.containsFullMask($evt.$ops, SelectionKey.OP_READ) && $a.$reader != null) $a.$reader = null; // gc help
+					if (Primitives.containsFullMask($evt.$ops, SelectionKey.OP_WRITE) && $a.$writer != null) $a.$writer = null; // gc help
+					if (Primitives.containsFullMask($evt.$ops, SelectionKey.OP_ACCEPT) && $a.$accepter != null) $a.$accepter = null; // gc help
+				} else if ($evt instanceof Event_Cancel) {
+					SelectionKey $k = getKey($evt);
+					if ($k != null) $k.cancel();
+					$k.attach(null); // gc help
+				}
+			}
+		}
+	}
+	
+	
+	
+	private class Attache {
+		public Listener<SelectableChannel>	$reader;
+		public Listener<SelectableChannel>	$writer;
+		public Listener<SelectableChannel>	$accepter;
+		
+		public void apply(Event $evt) {
+			switch ($evt.$ops) {
+				case SelectionKey.OP_READ:
+					$reader = $evt.$listener;
+					break;
+				case SelectionKey.OP_WRITE:
+					$writer = $evt.$listener;
+					break;
+				case SelectionKey.OP_ACCEPT:
+					$accepter = $evt.$listener;
+					break;
+				default:
+					throw new MajorBug("op type not supported");
+			}
+		}
+		
+		public boolean contains(Listener<SelectableChannel> $p) {
+			return ($reader == $p) || ($writer == $p) || ($accepter == $p);
 		}
 	}
 }
