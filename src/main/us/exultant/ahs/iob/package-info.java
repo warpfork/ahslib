cc137c9ee53ab582c4d6c0b6cd9310d03eb13bbf
hash <hash@exultant.us>
2012-05-28 12:30:39 -0700

    IOForge and related legacy IO convenience classes now exist in the us.exultant.ahs.iob package, and will be built as a part of the Core module's jar.

---
+++ src/main/us/exultant/ahs/iob/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>
+ * Simple tools to make basic IO less annoying.
+ * </p>
+ * 
+ * <p>
+ * This package ships as part of the <code>Core</code> module of AHSlib.
+ * </p>
+ */
+package us.exultant.ahs.iob;
+
