aeb311ce58f3f9d70ba2007b264ca9f650e7e994
hash <hash@exultant.us>
2012-05-30 22:48:06 -0700

    Add to ChainableInvocation documentation, describing meaning it may have when applied to static methods.

--- src/main/us/exultant/ahs/anno/ChainableInvocation.java
+++ src/main/us/exultant/ahs/anno/ChainableInvocation.java
@@ -3,9 +3,17 @@ package us.exultant.ahs.anno;
 import java.lang.annotation.*;
 
 /**
+ * <p>
  * Denotes that a method returns a pointer to the same object that the method was called
  * on. Methods with this type of signature are said to be supporting "chaining", because
  * it creates a distinctive form of syntactical sugar.
+ * </p>
+ * 
+ * <p>
+ * If used on a static method, it typically means that the first argument is the one
+ * returned. This is a common pattern with methods that build Strings by accepting a
+ * {@link StringBuilder} as a buffer, for example.
+ * </p>
  * 
  * @author Eric Myhre <tt>hash@exultant.us</tt>
  * 
