75634854f7010d4e28ad87ca4bcd810c3e25f429
hash <hash@exultant.us>
2013-04-24 15:08:34 -0500

    Add utility methods for indenting a string to the Strings class (it's not a clean or fast way to do things, but it's a terse way if you want it to format debugging or whathaveyou).  Also, whitespace: I'm no longer maintaining indents on blank lines, and I can't find a way to tell eclipse to only change lines I've touched, and I'm not going to do patchwise adds until kingdom come, so here it is.

--- src/main/us/exultant/ahs/util/Strings.java
+++ src/main/us/exultant/ahs/util/Strings.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2010 - 2013 Eric Myhre <http://exultant.us>
- * 
+ *
  * This file is part of AHSlib.
  *
  * AHSlib is free software: you can redistribute it and/or modify
@@ -27,114 +27,128 @@ public class Strings {
 	public static final Charset	UTF_8		= Charset.forName("UTF-8");
 	public static final Charset	ASCII		= Charset.forName("ASCII");
 	public static final char[]	HEX_CHARS	= new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-	
-	
-	
+
+
+
 //////////////////////////////////////////////////////////////// TRANSLATION FUCTIONS
 	// some of this functionality is already readily available, but these differ in that default charset always means utf-8
-	
+
 	public static final String fromBytes(byte[] $bats, Charset $cs) {
 		return new String($bats, $cs);
 	}
-	
+
 	public static final String fromBytes(byte[] $bats) {
 		return new String($bats, UTF_8);
 	}
-	
+
 	public static final String fromBytes(ByteBuffer $bats, Charset $cs) {
 		return new String(Arr.toArray($bats), $cs);
 	}
-	
+
 	public static final String fromBytes(ByteBuffer $bats) {
 		return new String(Arr.toArray($bats), UTF_8);
 	}
-	
+
 	public static final byte[] toBytes(String $s) {
 		return $s.getBytes(UTF_8);
 	}
-	
+
 //////////////////////////////////////////////////////////////// PARTING FUCTIONS
 	// default to returning the original string if the pattern is not found
-	
+
 	public static final String getPartAfter(String $source, String $pattern) {
 		int $index = $source.indexOf($pattern);
 		if ($index < 0) return $source;
 		return $source.substring($index+$pattern.length());
 	}
-	
+
 	public static final String getPartAfterOrEmpty(String $source, String $pattern) {
 		int $index = $source.indexOf($pattern);
 		if ($index < 0) return "";
 		return $source.substring($index+$pattern.length());
 	}
-	
+
 	public static final String getPartBefore(String $source, String $pattern) {
 		int $index = $source.indexOf($pattern);
 		if ($index < 0) return $source;
 		return $source.substring(0,$index);
 	}
-	
+
 	public static final String getPartBeforeOrEmpty(String $source, String $pattern) {
 		int $index = $source.indexOf($pattern);
 		if ($index < 0) return "";
 		return $source.substring(0,$index);
 	}
-	
+
 	public static final String getPartBetween(String $source, String $startPattern, String $endPattern) {
 		return getPartBeforeLast(getPartAfter($source,$startPattern),$endPattern);
 	}
-	
+
 	public static final String getPartAfterLast(String $source, String $pattern) {
 		int $index = $source.lastIndexOf($pattern);
 		if ($index < 0) return $source;
 		return $source.substring($index+$pattern.length());
 	}
-	
+
 	public static final String getPartAfterLastOrEmpty(String $source, String $pattern) {
 		int $index = $source.lastIndexOf($pattern);
 		if ($index < 0) return "";
 		return $source.substring($index+$pattern.length());
 	}
-	
+
 	public static final String getPartBeforeLast(String $source, String $pattern) {
 		int $index = $source.lastIndexOf($pattern);
 		if ($index < 0) return $source;
 		return $source.substring(0,$index);
 	}
-	
+
 	public static final String getPartBeforeLastOrEmpty(String $source, String $pattern) {
 		int $index = $source.lastIndexOf($pattern);
 		if ($index < 0) return "";
 		return $source.substring(0,$index);
 	}
-	
+
 	public static final String[] splitOnNext(String $source, String $pattern) {
 		int $index = $source.indexOf($pattern);
 		if ($index < 0) return new String[] {"",$source};
 		return new String[] {$source.substring(0,$index),$source.substring($index+$pattern.length())};
 	}
-	
+
 	public static final String[] splitOnLast(String $source, String $pattern) {
 		int $index = $source.lastIndexOf($pattern);
 		if ($index < 0) return new String[] {"",$source};
 		return new String[] {$source.substring(0,$index),$source.substring($index+$pattern.length())};
 	}
-	
+
 ////////////////////////////////////////////////////////////////
-	
+
 	public static final String merge(String[] $r) {
 		return merge($r,"\n");
 	}
-	
+
 	public static final String merge(String[] $r, String $dlim) {
 		StringBuilder $sb = new StringBuilder();
 		for (String $s : $r) $sb.append($s).append($dlim);
 		return $sb.toString();
 	}
-	
+
+	public static String indent(Object x) {
+		return indent(x, "\t");
+	}
+
+	public static String indent(Object x, String dent) {
+		String y = (x == null) ? "null" : x.toString();
+		boolean trailing = y.endsWith("\n");
+		String[] z = y.split("\n");
+		StringBuilder b = new StringBuilder(y.length()+z.length*dent.length());
+		for (String a : z)
+			b.append(dent).append(a).append('\n');
+		return b.substring(0, b.length() - (trailing ? 0 : 1));
+	}
+
 	/**
 	 * Mutates the given array of strings to contain only interned strings.
-	 * 
+	 *
 	 * @return an array of intern'd strings (=== the arg)
 	 */
 	public static final String[] intern(String[] $r) {
@@ -142,47 +156,47 @@ public class Strings {
 			$r[$i] = $r[$i].intern();
 		return $r;
 	}
-	
+
 	/**
 	 * @return an empty string if the argument was null; otherwise the argument.
 	 */
 	public static String noNull(String s) {
 		return (s == null ? "" : s);
 	}
-	
+
 //////////////////////////////////////////////////////////////// FILE NAME MANIPULATION FUNCTIONS
-	
+
 	public static final String dirname(String $path) {
 		if ($path.endsWith("/")) return $path;
 		return $path.substring(0, $path.lastIndexOf("/")+1);
 	}
-	
+
 	public static final String fullname(String $path) {
 		return getPartAfterLast($path, "/");
 	}
-	
+
 	public static final String basename(String $path) {
 		return getPartBefore(fullname($path),".");
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param $path a string representing a path for a filesystem
 	 * @return the string following the last period
 	 */
 	public static final String extension(String $path) {
 		return getPartAfterLastOrEmpty(fullname($path),".");
 	}
-	
+
 //////////////////////////////////////////////////////////////// PADDING FUNCTIONS
-	
+
 	public static String repeat(char $pad, int $count) {
 		if ($count < 0) throw new IllegalArgumentException("count must be >= 0");
 		char[] $ch = new char[$count];
 		Arr.fill($ch, $pad);
 		return new String($ch);
 	}
-	
+
 	public static String repeat(String $pad, int $count) {
 		if ($count < 0) throw new IllegalArgumentException("count must be >= 0");
 		StringBuffer $buf = new StringBuffer($count*$pad.length());
@@ -190,7 +204,7 @@ public class Strings {
 			$buf.append($pad);
 		return $buf.toString();
 	}
-	
+
 	/**
 	 * Buff a number with leading zeros, and return it as a string. (Effectively,
 	 * <code>padLeftToWidth(String.valueOf($n), '0', $desiredWidth)</code>.)
@@ -198,7 +212,7 @@ public class Strings {
 	public static String frontZeroBuff(int $n, int $desiredWidth) {
 		return padLeftToWidth(String.valueOf($n), '0', $desiredWidth);
 	}
-	
+
 	/**
 	 * Buff a number with leading zeros, and return it as a string. (Effectively,
 	 * <code>padLeftToWidth(String.valueOf($n), '0', $desiredWidth)</code>.)
@@ -206,10 +220,10 @@ public class Strings {
 	public static String frontZeroBuff(long $n, int $desiredWidth) {
 		return padLeftToWidth(String.valueOf($n), '0', $desiredWidth);
 	}
-	
+
 	/**
 	 * Appends $padCount space characters to the end of a string.
-	 * 
+	 *
 	 * @param $s
 	 * @param $padCount
 	 * @return the padded string
@@ -217,10 +231,10 @@ public class Strings {
 	public static String padRight(String $s, int $padCount) {
 		return padRight($s, ' ', $padCount);
 	}
-	
+
 	/**
 	 * Appends $padCount space characters to the beginning of a string.
-	 * 
+	 *
 	 * @param $s
 	 * @param $padCount
 	 * @return the padded string
@@ -228,10 +242,10 @@ public class Strings {
 	public static String padLeft(String $s, int $padCount) {
 		return padLeft($s, ' ', $padCount);
 	}
-	
+
 	/**
 	 * Appends $padCount iterations of the "$pad" string to the end of a string "$s".
-	 * 
+	 *
 	 * @param $s
 	 * @param $pad
 	 * @param $padCount
@@ -245,11 +259,11 @@ public class Strings {
 			$buf.append($pad);
 		return $buf.toString();
 	}
-	
+
 	/**
 	 * Appends $padCount iterations of the "$pad" string to the beginning of a string
 	 * "$s".
-	 * 
+	 *
 	 * @param $s
 	 * @param $pad
 	 * @param $padCount
@@ -263,10 +277,10 @@ public class Strings {
 		$buf.append($s);
 		return $buf.toString();
 	}
-	
+
 	/**
 	 * Appends $padCount iterations of the "$pad" string to the end of a string "$s".
-	 * 
+	 *
 	 * @param $s
 	 * @param $pad
 	 * @param $padCount
@@ -275,11 +289,11 @@ public class Strings {
 	public static String padRight(String $s, char $pad, int $padCount) {
 		return $s+repeat($pad,$padCount);
 	}
-	
+
 	/**
 	 * Appends $padCount iterations of the "$pad" string to the beginning of a string
 	 * "$s".
-	 * 
+	 *
 	 * @param $s
 	 * @param $pad
 	 * @param $padCount
@@ -288,12 +302,12 @@ public class Strings {
 	public static String padLeft(String $s, char $pad, int $padCount) {
 		return repeat($pad,$padCount)+$s;
 	}
-	
+
 	/**
 	 * If the string "$s" is shorter than $desiredWidth, space characters are
 	 * appeneded to the end of the string until it has a length matching
 	 * $desiredWidth.
-	 * 
+	 *
 	 * @param $s
 	 * @param $desiredWidth
 	 * @return the padded string
@@ -302,13 +316,13 @@ public class Strings {
 		if ($s.length() < $desiredWidth) return padRight($s, $desiredWidth - $s.length());
 		return $s;
 	}
-	
+
 	/**
 	 * If the string "$s" is shorter than $desiredWidth, the "$pad" string is appended
 	 * to the end of the string the number of times that would be necessary to make
 	 * the original string's length match $desiredWidth (assuming that the $pad string
 	 * is a single character in length).
-	 * 
+	 *
 	 * @param $s
 	 * @param $desiredWidth
 	 * @return the padded string
@@ -317,12 +331,12 @@ public class Strings {
 		if ($s.length() < $desiredWidth) return padRight($s, $pad, $desiredWidth - $s.length());
 		return $s;
 	}
-	
+
 	/**
 	 * If the string "$s" is shorter than $desiredWidth, space characters are
 	 * appeneded to the beginning of the string until it has a length matching
 	 * $desiredWidth.
-	 * 
+	 *
 	 * @param $s
 	 * @param $desiredWidth
 	 * @return the padded string
@@ -331,13 +345,13 @@ public class Strings {
 		if ($s.length() < $desiredWidth) return padLeft($s, $desiredWidth - $s.length());
 		return $s;
 	}
-	
+
 	/**
 	 * If the string "$s" is shorter than $desiredWidth, the "$pad" string is appended
 	 * to the beginning of the string the number of times that would be necessary to
 	 * make the original string's length match $desiredWidth (assuming that the $pad
 	 * string is a single character in length).
-	 * 
+	 *
 	 * @param $s
 	 * @param $desiredWidth
 	 * @return the padded string
@@ -346,7 +360,7 @@ public class Strings {
 		if ($s.length() < $desiredWidth) return padLeft($s, $pad, $desiredWidth - $s.length());
 		return $s;
 	}
-	
+
 	public static List<String> wrapToList(String $s, int $width) {
 		List<String> $v = new LinkedList<String>();
 		if (($s != null) && ($s.length() > 0)) {
@@ -385,9 +399,9 @@ public class Strings {
 		}
 		return $v;
 	}
-	
+
 //////////////////////////////////////////////////////////////// HEX ENCODING FUNCTIONS
-	
+
 	public static final byte[] fromHex(String $hex) { return decHex($hex); }
 	public static final byte[] decHex(String $hex) {
 		if ($hex == null) return null;
@@ -398,7 +412,7 @@ public class Strings {
 		}
 		return $bah;
 	}
-	
+
 	public static final String toHex(byte[] $bah) { return encHex($bah); }
 	public static final String encHex(byte[] $bah) {
 		if ($bah == null) return null;
@@ -409,9 +423,9 @@ public class Strings {
 		}
 		return new String($chars);
 	}
-	
+
 //////////////////////////////////////////////////////////////// OTHER CRAP
-	
+
 	/**
 	 * Returns a string of comma separated values. Bytes which are non-whitespace
 	 * printable ascii characters are printed as themselves prefixed by a single
